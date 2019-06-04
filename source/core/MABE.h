f6f53c8197b34251304ae3d37f50073d0b793556
Charles Ofria <charles.ofria@gmail.com>
2019-06-04 14:27:26 -0400

    Added a main MABE controller object.

---
+++ source/core/MABE.h
@@ -0,0 +1,22 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere/MABE
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  MABE.h
+ *  @brief Master controller object for a MABE run.
+ */
+
+#ifndef MABE_POPULATION_H
+#define MABE_POPULATION_H
+
+#include "Population.h"
+
+namespace mabe {
+
+  class MABE {
+  };
+
+}
+
+#endif
