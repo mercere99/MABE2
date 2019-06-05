37dd38d5c441a1b9e9485222620001a311b4c029
Charles Ofria <charles.ofria@gmail.com>
2019-06-05 17:21:31 -0400

    Fleshed out accessors for World.

--- source/core/World.h
+++ source/core/World.h
@@ -14,19 +14,49 @@
 #ifndef MABE_WORLD_H
 #define MABE_WORLD_H
 
+#include <string>
+
 #include "base/Ptr.h"
 #include "base/vector.h"
+#include "tools/vector_utils.h"
 
-#include "EvaluateModule.h"
 #include "Population.h"
+#include "EvaluateModule.h"
 #include "SelectModule.h"
 
 namesapce mabe {
 
   class World {
+  private:
+    std::string name;
+
     emp::vector<mabe::Population> pops;
     emp::vector<emp::Ptr<EvaluateModule>> evals;
     emp::vector<emp::Ptr<SelectModule>> selects;
+
+  public:
+    World(const std::string & in_name) : name(in_name) { ; }
+    World(const World &) = default;
+    World(World &&) = default;
+
+    const std::string & GetName() const { return name; }
+    
+    int GetPopID(const std::string & pop_name) const {
+      return emp::FindEval(pops, [pop_name](auto p){ return p.GetName() == pop_name; });
+    }
+    int GetEvalID(const std::string & e_name) const {
+      return emp::FindEval(evals, [e_name](auto e){ return e->GetName() == e_name; });
+    }
+    int GetSelectID(const std::string & s_name) const {
+      return emp::FindEval(selects, [s_name](auto s){ return s->GetName() == s_name; });
+    }
+
+    const Population & GetPopulation(int id) const { return pops[(size_t) id]; }
+    const EvaluateModule & GetEvaluateModule(int id) const { return evals[(size_t) id]; }
+    const SelectModule & GetSelectModule(int id) const { return selects[(size_t) id]; }
+    Population & GetPopulation(int id) { return pops[(size_t) id]; }
+    EvaluateModule & GetEvaluateModule(int id) { return evals[(size_t) id]; }
+    SelectModule & GetSelectModule(int id) { return selects[(size_t) id]; }
   };
 
 }
