119ea176e4eefda6b9ae085984824f7778d26b59
Charles Ofria <charles.ofria@gmail.com>
2019-06-05 23:01:48 -0400

    Setup constructors and name for Popilation object.

--- source/core/Population.h
+++ source/core/Population.h
@@ -1,5 +1,5 @@
 /**
- *  @note This file is part of MABE, https://github.com/mercere/MABE
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
  *  @date 2019
  *
@@ -13,6 +13,9 @@
 #ifndef MABE_POPULATION_H
 #define MABE_POPULATION_H
 
+#include <string>
+
+#include "base/Ptr.h"
 #include "base/vector.h"
 
 #include "Organism.h"
@@ -21,12 +24,21 @@ namespace mabe {
 
   class Population {
   private:
-    emp::vector<Organism> orgs;
+    std::string name;
+    emp::vector<emp::Ptr<Organism>> orgs;
   public:
     class Position {
       emp::Ptr<Population> pop_ptr;
       size_t pos;
     };
+
+    Population(const std::string & in_name) : name(in_name) { }
+    Population(const Population &) = delete;
+    Population(Population &&) = default;
+
+    ~Population() { for (auto x : orgs) x.Delete(); }
+
+    const std::string & GetName() const { return name; }
   };
 
 }
