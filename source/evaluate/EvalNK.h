9be5f55388e895159b0e8b79227c3262600aeda1
Charles Ofria <charles.ofria@gmail.com>
2019-06-15 14:03:42 -0400

    Added a new start on a MABE NK evaluate module.

---
+++ source/evaluate/EvalNK.h
@@ -0,0 +1,20 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  EvalNK.h
+ *  @brief MABE Evaluation module for NK Landscapes
+ */
+
+#include "../tools/NK.h"
+
+#ifndef MABE_EVAL_NK_H
+#define MABE_EVAL_NK_H
+
+class EvalNK {
+private:
+public:
+};
+
+#endif
