26a25f18bbf860477813b6d8720d1766e4856905
Charles Ofria <charles.ofria@gmail.com>
2019-06-24 23:07:57 -0400

    Added in a VarMap for OrganismType and mutation functions.

--- source/core/OrganismType.h
+++ source/core/OrganismType.h
@@ -10,27 +10,39 @@
 #ifndef MABE_ORGANISM_TYPE_H
 #define MABE_ORGANISM_TYPE_H
 
+#include <functional>
+
 #include "base/unordered_map.h"
 
 namespace mabe {
 
+  class Organism;
+
   class OrgTypeBase {
   private:
-    std::string name;
+    std::string name;     ///< Name used for this type of organisms.
+    emp::VarMap var_map;  ///< Map of run-time values associated with this organism type.
 
   public:
     OrgTypeBase(const std::string & in_name) : name(in_name) { ; }
     virtual ~OrgTypeBase() { ; }
 
     const std::string & GetName() const { return name; }
+
+    virtual size_t MutateOrg(Organism & org) = 0;
   };
 
   template <typename ORG_T>
   class OrganismType : public OrgTypeBase {
   private:
+    std::function<size_t(ORG_T &)> mut_fun;  // Function to mutate this type of organism.
 
   public:
-    OrganismType(const std::string & in_name) : OrgTypeBase(in_name) { ; }
+    OrganismType(const std::string & in_name) : OrgTypeBase(in_name) {
+      mut_fun = [](ORG_T &){ return 0; }; // No mutation function setup!
+    }
+
+    size_t MutateOrg(Organism & org) { return mut_fun(org); }
   };
 
 }
