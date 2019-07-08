f9c3764be9d38c20ba88ff9ac604ff6d0c6c7efd
Charles Ofria <charles.ofria@gmail.com>
2019-07-07 23:17:23 -0400

    Updated EvalNK with MABE interface changes.

--- source/evaluate/EvalNK.h
+++ source/evaluate/EvalNK.h
@@ -21,18 +21,21 @@ namespace mabe {
   class EvalNK : public Module {
   private:
     size_t N;
-    size_t K;
+    size_t K;    
     NKLandscape landscape;
+    size_t target_pop;
+
     std::string bits_trait;
     std::string fitness_trait;
 
   public:
     EvalNK(size_t _N, size_t _K, const std::string & _btrait="NK", const std::string & _ftrait="fitness")
-      : N(_N), K(_K), bits_trait(_btrait), fitness_trait(_ftrait)
+      : Module("EvalNK"), N(_N), K(_K), target_pop(0), bits_trait(_btrait), fitness_trait(_ftrait)
     {
       IsEvaluate(true);
       AddOwnedTrait<emp::BitVector>(bits_trait, "NK Bit Sequence", emp::BitVector());
       AddOwnedTrait<double>(fitness_trait, "NK fitness value", 0.0);
+      SetMinPops(1);
     }
     ~EvalNK() { }
 
@@ -40,15 +43,13 @@ namespace mabe {
       landscape.Config(N, K, world.GetRandom());  // Setup the fitness landscape.
     }
 
-    void Update() {
-      // Loop through the populations and evaluate each organism.
-      for (auto & pop : pops) {
-        for (auto & org : pop) {
-          org.GenerateOutput(bits_trait);
-          double fitness = landscape.GetFitness( org.GetVar<emp::BitVector>(bits_trait) );
-          org.SetVar<double>(fitness_trait, fitness);
-        }
-      }
+    void Update(mabe::World & world) {
+      // Loop through the population and evaluate each organism.
+      for (auto & org : world.GetPopulation(0)) {
+        org.GenerateOutput(bits_trait);
+        double fitness = landscape.GetFitness( org.GetVar<emp::BitVector>(bits_trait) );
+        org.SetVar<double>(fitness_trait, fitness);
+      }      
     }
   };
 
