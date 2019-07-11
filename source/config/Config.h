ca20d99c762773870b4fca82ea592d8eeb12fd47
Charles Ofria <charles.ofria@gmail.com>
2019-07-10 23:41:40 -0400

    Started to build configuration manager.

---
+++ source/config/Config.h
@@ -0,0 +1,28 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  Config.h
+ *  @brief Manages all configuration of MABE runs.
+ *  @note Status: ALPHA
+ */
+
+#ifndef MABE_CONFIG_H
+#define MABE_CONFIG_H
+
+#include "base/assert.h"
+#include "meta/TypeID.h"
+#include "tools/string_utils.h"
+
+namespace mabe {
+
+  class Config {
+  protected:
+
+  public:
+
+  };
+
+}
+#endif
