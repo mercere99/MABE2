7b13a58200ce90c0d5864d694e18c0658e53486d
Charles Ofria <charles.ofria@gmail.com>
2019-07-11 13:35:53 -0400

    Updated specs in Config.h; included ConfigEntry.h

--- source/config/Config.h
+++ source/config/Config.h
@@ -8,33 +8,34 @@
  *  @note Status: ALPHA
  * 
  *  Example usage:
- *   a = 7              # a is a variable with the value 7
- *   b = "balloons"     # b is a variable equal to the literal string "balloons"
- *   c = a + 10         # '+' will add values; c is a variable equal to 17.
- *   d = "99 " + b      # '+' will append strings; d is a variable equal to "99 balloons"
- *   # e = "abc" + 123  # ERROR - cannot add strings and values!
- *   f = {              # f is a structure/scope/dictionary
- *     g = 1
- *     h = "two"
+ *   a = 7;              # a is a variable with the value 7
+ *   b = "balloons";     # b is a variable equal to the literal string "balloons"
+ *   c = a + 10;         # '+' will add values; c is a variable equal to 17.
+ *   d = "99 " + b;      # '+' will append strings; d is a variable equal to "99 balloons"
+ *   # e = "abc" + 123;  # ERROR - cannot add strings and values!
+ *   f = {               # f is a structure/scope/dictionary
+ *     g = 1;
+ *     h = "two";
  *     i = {
- *       j = 3
+ *       j = 3;
  *     }
- *     a = "shadow!"    # A variable can be redeclared in other scopes, shadowing the original.
- *     j = "spooky!"    # A NEW variable since we are out of the namespace of the other j.
- *     b = i.j          # Namespaces can be stepped through with dots.
- *     c = .b           # A variable name beginning with a dot indicates global namespace.
- *   }                  # f has been initialized with seven variables in its scope.
- *   k = [ 1 , 2 , 3]   # k is a vector of values (vectors must have all types the same!)
- *   l = k[1]           # Vectors can be indexed into.
- *   m() = a * c        # Functions have parens after the variable name; evaluated when called.
- *   n(o,p) = o + p     # Functions may have arguments.
- *   q = 'q'            # Literal chars are translated immediately to their ascii value
+ *     a = "shadow!";    # A variable can be redeclared in other scopes, shadowing the original.
+ *     j = "spooky!";    # A NEW variable since we are out of the namespace of the other j.
+ *     b = i.j;          # Namespaces can be stepped through with dots.
+ *     c = .b;           # A variable name beginning with a dot indicates global namespace.
+ *   }                   # f has been initialized with seven variables in its scope.
+ *   f["new"] = 22;      # You can always add new fields to structures.
+ *   k = [ 1 , 2 , 3];   # k is a vector of values (vectors must have all types the same!)
+ *   l = k[1];           # Vectors can be indexed into.
+ *   m() = a * c;        # Functions have parens after the variable name; evaluated when called.
+ *   n(o,p) = o + p;     # Functions may have arguments.
+ *   q = 'q';            # Literal chars are translated immediately to their ascii value
  * 
  *   # There are a few pre-defined functions at the global namespace.
- *   r = size(k)        # = 3  (always a value)
- *   s = names(f)       # = ["a","b","c","g","h","i","j"] (vector of strings in alphabetical order)
- *   t = string(c)      # = "17"  (convert value to string)
- *   u = value(t+"00")  # = 1700  (convert string to value)
+ *   r = size(k);        # = 3  (always a value)
+ *   s = names(f);       # = ["a","b","c","g","h","i","j"] (vector of strings in alphabetical order)
+ *   t = string(c);      # = "17"  (convert value to string)
+ *   u = value(t+"00");  # = 1700  (convert string to value)
  * 
  * 
  *  In practice:
@@ -43,19 +44,19 @@
  *       class = MarkovBrain;
  *       outputs = 10;
  *       node_weights = 0.75;
- *       recurrance = 5
+ *       recurrance = 5;
  *     }
  *     Wolves = {
  *       class = MarkovBrain;
  *       outputs = 10;
  *       node_weights = 0.75;
- *       recurrance = 3
+ *       recurrance = 3;
  *     }
  *   }
  *   modules = {
  *     Mutations = {
- *       copy_prob = 0.001
- *       insert_prob = 0.05
+ *       copy_prob = 0.001;
+ *       insert_prob = 0.05;
  *     }
  *   }
  */
@@ -66,6 +67,8 @@
 #include "base/assert.h"
 #include "meta/TypeID.h"
 #include "tools/string_utils.h"
+
+#include "ConfigEntry.h"
 
 namespace mabe {
 
