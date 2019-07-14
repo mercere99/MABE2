15fc8ab416357824ad7bd978881ca60d547d8abf
Charles Ofria <charles.ofria@gmail.com>
2019-07-13 23:34:28 -0400

    Setup ConfigLink to have an override for strings.

--- source/config/ConfigLink.h
+++ source/config/ConfigLink.h
@@ -51,9 +51,27 @@ namespace mabe {
 
     void Write(std::ostream & os=std::cout, const std::string & prefix="") const override {
       if (desc.size()) os << prefix << "# " << desc << "\n";
-      os << prefix << name << " = " << emp::to_string(linked_var) << ";\n\n";
+      os << prefix << name << " = " << emp::to_string(linked_var) << ";\n";
     }
+  };
 
+  /// Specialized version of ConfigLink for Strings (Which need to be output as a string literal)
+  template <>
+  class ConfigLink<std::string> : public ConfigLink_Base {
+  protected:
+    std::string & linked_var;   ///< A reference to the variable linked to this configuration option.
+  public:
+    ConfigLink(std::string & var, const std::string & name, const std::string & desc="")
+      : ConfigLink_Base(name, desc), linked_var(var) { }
+    ~ConfigLink() { }
+
+    void SetValue(double val) override { linked_var = val; }
+    void SetString(const std::string & val) override { linked_var = emp::from_string<T>(val); }
+
+    void Write(std::ostream & os=std::cout, const std::string & prefix="") const override {
+      if (desc.size()) os << prefix << "# " << desc << "\n";
+      os << prefix << name << " = " << emp::to_literal(linked_var) << ";\n";
+    }
   };
 
 }
