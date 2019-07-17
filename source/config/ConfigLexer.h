b39f66c8826aff8a8a9916e9142b2c5c915bfe8a
Charles Ofria <charles.ofria@gmail.com>
2019-07-16 23:42:56 -0400

    Added a lexer for config files.

---
+++ source/config/ConfigLexer.h
@@ -0,0 +1,49 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  ConfigLexer.h
+ *  @brief A Lexer that tokenizes MABE config files.
+ **/
+
+#ifndef MABE_CONFIG_LEXER_H
+#define MABE_CONFIG_LEXER_H
+
+#include "tools/Lexer.h"
+
+namespace mabe {
+
+  class ConfigLexer : public emp::Lexer {
+  private:
+    int token_identifier = -1;        ///< Token id for identifiers.
+    int token_number = -1;            ///< Token id for literal numbers.
+    int token_string = -1;            ///< Token id for literal strings.
+    int token_char = -1;              ///< Token id for literal characters.
+    int token_symbol = -1;            ///< Token id for other symbols.
+
+  public:
+    ConfigLexer() {
+      // Whitespace and comments should always be dismissed (top priority)
+      IgnoreToken("Whitespace", "[ \t\n\r]+");
+      IgnoreToken("//-Comments", "//.*");
+      IgnoreToken("/*...*/-Comments", "/[*]([^*]|([*]+[^*/]))*[*]+/");
+
+      // Meaningful tokens have next priority.
+      token_identifier = AddToken("Identifier", "[a-zA-Z_][a-zA-Z0-9_]*");
+      token_number = AddToken("Literal Number", "[0-9]+(.[0-9]+)?");
+      token_string = AddToken("Literal String", "\\\"([^\"\\\\]|\\\\.)*\\\"");
+      token_char = AddToken("Literal Character", "'([^'\n\\\\]|\\\\.)+'");  // C++ will throw errors for bad usage.
+
+      // Symbol tokens should have least priority.
+      token_symbol = AddToken("Symbol", ".|\"::\"|\"==\"|\"!=\"|\"<=\"|\">=\"|\"->\"|\"&&\"|\"||\"|\"<<\"|\">>\"|\"++\"|\"--\"");
+    }
+
+    bool IsID(const emp::Token token) const { return token.token_id == token_identifier; }
+    bool IsNumber(const emp::Token token) const { return token.token_id == token_number; }
+    bool IsString(const emp::Token token) const { return token.token_id == token_string; }
+    bool IsSymbol(const emp::Token token) const { return token.token_id == token_symbol; }
+  };
+}
+
+#endif
