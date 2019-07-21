ba4d2f77c42c81e3e7adae06273120c5310aba18
Charles Ofria <charles.ofria@gmail.com>
2019-07-21 05:33:25 -0400

    Added dots as a special token in ConfigLexer.

--- source/config/ConfigLexer.h
+++ source/config/ConfigLexer.h
@@ -16,11 +16,12 @@ namespace mabe {
 
   class ConfigLexer : public emp::Lexer {
   private:
-    int token_identifier = -1;        ///< Token id for identifiers.
-    int token_number = -1;            ///< Token id for literal numbers.
-    int token_string = -1;            ///< Token id for literal strings.
-    int token_char = -1;              ///< Token id for literal characters.
-    int token_symbol = -1;            ///< Token id for other symbols.
+    int token_identifier = -1;        ///< Token id for identifiers
+    int token_number = -1;            ///< Token id for literal numbers
+    int token_string = -1;            ///< Token id for literal strings
+    int token_char = -1;              ///< Token id for literal characters
+    int token_dots = -1;              ///< Token id for a series of dots (...)
+    int token_symbol = -1;            ///< Token id for other symbols
 
   public:
     ConfigLexer() {
@@ -33,16 +34,20 @@ namespace mabe {
       token_identifier = AddToken("Identifier", "[a-zA-Z_][a-zA-Z0-9_]*");
       token_number = AddToken("Literal Number", "[0-9]+(.[0-9]+)?");
       token_string = AddToken("Literal String", "\\\"([^\"\\\\]|\\\\.)*\\\"");
-      token_char = AddToken("Literal Character", "'([^'\n\\\\]|\\\\.)+'");  // C++ will throw errors for bad usage.
+      token_char = AddToken("Literal Character", "'([^'\n\\\\]|\\\\.)+'");
+      token_dots = AddToken("Dots", "\".\"+");
 
-      // Symbol tokens should have least priority.
+      /// Symbol tokens should have least priority.  They include any solitary character not listed
+      /// above, or pre-specified multi-character groups.
       token_symbol = AddToken("Symbol", ".|\"::\"|\"==\"|\"!=\"|\"<=\"|\">=\"|\"->\"|\"&&\"|\"||\"|\"<<\"|\">>\"|\"++\"|\"--\"");
     }
 
-    bool IsID(const emp::Token token) const { return token.token_id == token_identifier; }
-    bool IsNumber(const emp::Token token) const { return token.token_id == token_number; }
-    bool IsString(const emp::Token token) const { return token.token_id == token_string; }
-    bool IsSymbol(const emp::Token token) const { return token.token_id == token_symbol; }
+    bool IsID(const emp::Token token) const noexcept { return token.token_id == token_identifier; }
+    bool IsNumber(const emp::Token token) const noexcept { return token.token_id == token_number; }
+    bool IsString(const emp::Token token) const noexcept { return token.token_id == token_string; }
+    bool IsChar(const emp::Token token) const noexcept { return token.token_id == token_char; }
+    bool IsDots(const emp::Token token) const noexcept { return token.token_id == token_dots; }
+    bool IsSymbol(const emp::Token token) const noexcept { return token.token_id == token_symbol; }
   };
 }
 
