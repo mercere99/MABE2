aecc7292b034726f3a564137fd09c424fafce9a1
Charles Ofria <charles.ofria@gmail.com>
2019-07-29 18:08:11 -0400

    Removed ConfigScope from ConfigEntry (now that it has its own file)

--- source/config/ConfigEntry.h
+++ source/config/ConfigEntry.h
@@ -17,7 +17,6 @@
 #define MABE_CONFIG_ENTRY_H
 
 #include "base/assert.h"
-#include "base/map.h"
 #include "base/Ptr.h"
 #include "base/vector.h"
 #include "meta/TypeID.h"
@@ -214,110 +213,6 @@ namespace mabe {
     ConfigEntry & SetString(const std::string & in) override { value = in; return *this; }
 
     bool CopyValue(const ConfigEntry & in) override { value = in.AsString(); return true; }
-  };
-
-  // Set of multiple config entries.
-  class ConfigScope : public ConfigEntry {
-  protected:
-    emp::map< std::string, emp::Ptr<ConfigEntry> > entries;
-
-    template <typename T, typename... ARGS>
-    T & Add(const std::string & name, ARGS &&... args) {
-      auto new_ptr = emp::NewPtr<T>(name, std::forward<ARGS>(args)...);
-      entries[name] = new_ptr;
-      return *new_ptr;
-    }
-  public:
-    ConfigScope(const std::string & _name,
-                 const std::string & _desc,
-                 emp::Ptr<ConfigScope> _scope)
-      : ConfigEntry(_name, _desc, _scope) { }
-    ConfigScope(const ConfigScope & in) : ConfigEntry(in) {
-      for (const auto & x : in.entries) {
-        entries[x.first] = x.second->Clone();
-      }
-    }
-    ConfigScope(ConfigScope &&) = default;
-
-    ~ConfigScope() {
-      // Clear up all entries.
-      for (auto & x : entries) { x.second.Delete(); }
-    }
-
-    emp::Ptr<ConfigScope> AsScopePtr() override { return this; }
-
-    void UpdateDefault() override {
-      // Recursively update all defaults within the structure.
-      for (auto & x : entries) x.second->UpdateDefault();
-      default_val = ""; /* @CAO: Need to spell out? */
-    }
-
-    // Get an entry out of this scope; 
-    emp::Ptr<ConfigEntry> GetEntry(std::string in_name) {
-      // Lookup this next entry is in the var list.
-      auto it = entries.find(in_name);
-
-      // If this name is unknown, fail!
-      if (it == entries.end()) return nullptr;
-
-      // Otherwise return the entry.
-      return it->second;
-    }
-
-    // Lookup a variable, scanning outer scopes if needed
-    emp::Ptr<ConfigEntry> LookupEntry(std::string in_name, bool scan_scopes=true) override {
-      // See if this next entry is in the var list.
-      auto it = entries.find(in_name);
-
-      // If this name is unknown, check with the parent scope!
-      if (it == entries.end()) {
-        if (scope.IsNull() || !scan_scopes) return nullptr;  // No parent?  Just fail...
-        return scope->LookupEntry(in_name);
-      }
-
-      // Otherwise we found it!
-      return it->second;
-    }
-
-    // Lookup a variable, scanning outer scopes if needed (in constant context!)
-    emp::Ptr<const ConfigEntry> LookupEntry(std::string in_name, bool scan_scopes=true) const override {
-      // See if this entry is in the var list.
-      auto it = entries.find(in_name);
-
-      // If this name is unknown, check with the parent scope!
-      if (it == entries.end()) {
-        if (scope.IsNull() || !scan_scopes) return nullptr;  // No parent?  Just fail...
-        return scope->LookupEntry(in_name);
-      }
-
-      // Otherwise we found it!
-      return it->second;
-    }
-
-    template <typename VAR_T, typename DEFAULT_T>
-    ConfigEntry_Linked<VAR_T> & LinkVar(VAR_T & var,
-                                    const std::string & name,
-                                    const std::string & desc,
-                                    DEFAULT_T default_val) {
-      return Add<ConfigEntry_Linked<VAR_T>>(name, var, desc, this);
-    }
-
-    ConfigScope & AddScope(const std::string & name, const std::string & desc) {
-      return Add<ConfigScope>(name, desc, this);
-    }
-
-    ConfigEntry & Write(std::ostream & os=std::cout, const std::string & prefix="") override {
-      if (desc.size()) os << prefix << "// " << desc << "\n";
-
-      os << prefix << name << " = {\n";
-      for (auto & x : entries) {
-        x.second->Write(os, prefix+"  ");
-      }
-      os << prefix << "}\n";
-      return *this;
-    }
-
-    emp::Ptr<ConfigEntry> Clone() const override { return emp::NewPtr<ConfigScope>(*this); }
   };
 
 }
