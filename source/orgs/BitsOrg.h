0c96b0c599c93f9a5ff1501001435831489d0f70
Charles Ofria <charles.ofria@gmail.com>
2019-08-04 14:42:05 -0400

    Added BitsOrgType to BitsOrg.

--- source/orgs/BitsOrg.h
+++ source/orgs/BitsOrg.h
@@ -12,43 +12,28 @@
 #define MABE_BITS_ORGANISM_H
 
 #include "../core/Organism.h"
+#include "../core/OrganismType_Wrapper.h"
 
 #include "tools/BitVector.h"
+#include "tools/random_utils.h"
 
 namespace mabe {
 
   class BitsOrg : public Organism {
+    friend class BitsOrgType;
   protected:
     emp::BitVector bits;
 
   public:
-    BitsOrg() { ; }
+    BitsOrg(emp::Ptr<OrganismType> ptr) : Organism(ptr) { ; }
     BitsOrg(const BitsOrg &) = default;
     BitsOrg(BitsOrg &&) = default;
-    BitsOrg(const emp::BitVector & in) : bits(in) { ; }
-    BitsOrg(size_t N) : bits(N) { ; }
+    BitsOrg(const emp::BitVector & in, emp::Ptr<OrganismType> ptr) : Organism(ptr), bits(in) { ; }
+    BitsOrg(size_t N, emp::Ptr<OrganismType> ptr) : Organism(ptr), bits(N) { ; }
     ~BitsOrg() { ; }
-
-    /// Setup the variables that can change with different types of this organism.
-    static void SetupType(OrganismType & type) {
-      type.AddVar<size_t>("N", "Number of bits in organism", 1);
-    }
-
-    /// Use the default constructor for cloning.
-    emp::Ptr<Organism> Clone() const override { return emp::NewPtr<BitsOrg>(*this); }
 
     /// Use "to_string" to convert.
     std::string ToString() override { return emp::to_string(bits); }
-
-    /// Don't do mutations unless a mutate function has been set.
-    int Mutate(emp::Random & random) override {
-      if (bits.size() == 0) return 0;
-      for (size_t i = 0; i < 3; i++) {
-        size_t pos = random.GetUInt(bits.size());
-        bits[pos] = random.P(0.5);
-      }
-      return 3;
-    }
 
     /// Just use the bit sequence as the output.
     void GenerateOutput(const std::string & output_name="result", size_t=0) override {
@@ -60,6 +45,42 @@ namespace mabe {
     virtual emp::TypeID GetOutputType(size_t=0) override {
       return emp::GetTypeID<emp::BitVector>();
     }
+  };
+
+  class BitsOrgType : public OrganismType_Wrapper<BitsOrg> {
+  private:
+    size_t N;  ///< Number of bits in this type of organism.
+    using base_t = OrganismType_Wrapper<BitsOrg>;
+  public:
+    BitsOrgType(const std::string & in_name) : base_t(in_name) { ; }
+
+    emp::Ptr<Organism> MakeOrganism(emp::Random & random) const override {
+      auto org_ptr = emp::NewPtr<BitsOrg>(N, this);
+      Randomize(*org_ptr, random);
+      return org_ptr;
+    }
+
+    size_t Mutate(Organism & org, emp::Random & random) const override {
+      auto & bits = ConvertOrg(org).bits;
+
+      if (bits.size() == 0) return 0;
+      for (size_t i = 0; i < 3; i++) {
+        size_t pos = random.GetUInt(bits.size());
+        bits[pos] = random.P(0.5);
+      }
+      return 3;
+    }
+
+    void Randomize(Organism & org, emp::Random & random) const override {
+      emp::RandomizeBitVector(ConvertOrg(org).bits, random, 0.5);
+    }
+
+    std::ostream & Print(Organism & org, std::ostream & os) const override {
+      emp_assert(&(org.GetType()) == this);
+      os << org.ToString();
+      return os;
+    }
+
   };
 
 }
