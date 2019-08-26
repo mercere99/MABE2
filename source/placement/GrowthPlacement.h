00010810d98634e5b17547650330ec539621a90d
Charles Ofria <charles.ofria@gmail.com>
2019-08-26 18:43:18 -0400

    Minor cleanup in growth placement.

--- source/placement/GrowthPlacement.h
+++ source/placement/GrowthPlacement.h
@@ -7,8 +7,8 @@
  *  @brief Default placement using one or two unstructured populations, "main" and "next".
  */
 
-#ifndef MABE_Growth_PLACEMENT_H
-#define MABE_Growth_PLACEMENT_H
+#ifndef MABE_GROWTH_PLACEMENT_H
+#define MABE_GROWTH_PLACEMENT_H
 
 #include "../core/MABE.h"
 #include "../core/Module.h"
@@ -17,8 +17,8 @@ namespace mabe {
 
   class GrowthPlacement : public Module {
   private:
-    int from_pop=0;
-    int to_pop=1;
+    int main_pop=0;
+    int next_pop=1;
 
   public:
     GrowthPlacement(mabe::MABE & control)
@@ -45,20 +45,21 @@ namespace mabe {
       (void) org;  // By default, organism doesn't matter.
 
       // If birth is not coming from monitored population, don't place!
-      if (ppos.PopID() != from_pop) return OrgPosition();
-      return control.PushEmpty(control.GetPopulation(to_pop));  // @CAO Should use next_pop...
+      if (ppos.PopID() != main_pop) return OrgPosition();
+      return control.PushEmpty(control.GetPopulation(next_pop));  // @CAO Should use next pop...
     }
 
+    // Injections always go into the active population.
     OrgPosition DoPlaceInject(Organism & org) override {
       (void) org;  // By default, organism doesn't matter.
-      return control.PushEmpty(control.GetPopulation(to_pop));  // @CAO Should use next_pop...
+      return control.PushEmpty(control.GetPopulation(main_pop));  // @CAO Should use main pop...
     }
 
     OrgPosition DoFindNeighbor(OrgPosition pos) override {
       emp::Ptr<Population> pop_ptr = pos.PopPtr();
 
       // If the current position is either not a population or not one monitored, don't find!
-      if (pop_ptr.IsNull() || pos.PopID() != from_pop) return OrgPosition();
+      if (pop_ptr.IsNull() || pos.PopID() != main_pop) return OrgPosition();
       return OrgPosition(pop_ptr, control.GetRandom().GetUInt(pop_ptr->GetSize()));
     }
 
