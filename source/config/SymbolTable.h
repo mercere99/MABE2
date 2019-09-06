876612db5b14881c54f9a219c750a4db84126b57
Charles Ofria <charles.ofria@gmail.com>
2019-09-06 09:45:00 -0400

    Building out type handling in SymbolTable.

--- source/config/SymbolTable.h
+++ source/config/SymbolTable.h
@@ -19,29 +19,80 @@
 
 namespace mabe {
 
+  /// General type information for variables in a scripting language.
   class TypeInfo {
+  private:
+    std::string name;
+
+  public:
+    TypeInfo(const std::string & in_name) : name(in_name) { ; }
+  };
+
+  /// Specialty type information for when we are working with strings.
+  class TypeInfo_String : public TypeInfo {
+  private:
+    std::string name;
+
+  public:
+    TypeInfo_String(const std::string & in_name) : TypeInfo(in_name) { ; }
+  };
+
+  /// Specialty type information for when we are working with numeric values.
+  template <typename T>
+  class TypeInfo_Numeric : public TypeInfo {
+  private:
+    std::string name;
+
+  public:
+    TypeInfo_Numeric(const std::string & in_name) : TypeInfo(in_name) { ; }
+  };
+
+  /// Specialty type information for when we are working with a more complex structure.
+  template <typename T>
+  class TypeInfo_Struct : public TypeInfo {
+  private:
+    std::string name;
+
+  public:
+    TypeInfo_Struct(const std::string & in_name) : TypeInfo(in_name) { ; }
   };
 
   class SymbolInfo {
   private:
+    std::string name;
     const TypeInfo & type;
 
-  protected:
-    SymbolInfo(const TypeInfo & in_type) : type(in_type) { ; }
+  public:
+    SymbolInfo(const std::string & in_name,
+               const TypeInfo & in_type)
+    : name(in_name), type(in_type) { ; }
     ~SymbolInfo() { ; }
   };
 
   class SymbolTable {
   private:
-    emp::map<std::string, TypeInfo> type_map;
+    emp::map<std::string, emp::Ptr<TypeInfo>> type_map;
   public:
     SymbolTable() { ; }
     ~SymbolTable() { ; }
 
-    // Simple type handling.
-    TypeInfo & AddType(const std::string & name) { return type_map[name]; }
-    TypeInfo & GetType(const std::string & name) { return type_map[name]; }
+    // --- Simple type handling ---
+
+    TypeInfo & GetType(const std::string & name) {
+      emp_assert(HasType(name), "Trying to get type that does not exist.", name);
+      return *type_map[name];
+    }
+
     bool HasType(const std::string & name) { return emp::Has(type_map, name); }
+
+    template <typename T>
+    TypeInfo & AddType(const std::string & name) {
+      emp_assert(!HasType(name), "Trying to add type that already exists.", name);
+      type_map[name] = new TypeInfo_Struct<T>(name);
+      return *type_map[name];
+    }
+
+    // --- Simple variable handling ---
 
     void AddSymbol() { ; }
   };
