b3ecc6fab858826647279c944dddc4c98e0ffaed
Charles Ofria <charles.ofria@gmail.com>
2019-09-26 17:23:44 -0400

    Moved helper functions that link vars/funs to config options into ConfigType.

--- source/config/ConfigType.h
+++ source/config/ConfigType.h
@@ -13,6 +13,9 @@
 
 #include "base/assert.h"
 
+#include "ConfigEntry.h"
+#include "ConfigScope.h"
+
 namespace mabe {
 
   enum class BaseType {
@@ -23,13 +26,36 @@ namespace mabe {
     STRUCT
   };
 
-  class ConfigScope;
-
   // Abstract base class for types that we want to be used for scripting..
   class ConfigType {
   private:
     emp::Ptr<ConfigScope> cur_scope;
   
+  public:
+    
+    // ---==  Configuration Management ==---
+
+    /// Link a variable to a configuration entry - it sets the new default and
+    /// automatically updates when configs are loaded.
+    template <typename VAR_T, typename DEFAULT_T>
+    ConfigEntry_Linked<VAR_T> & LinkVar(VAR_T & var,
+                                        const std::string & name,
+                                        const std::string & desc,
+                                        DEFAULT_T default_val) {
+      return GetScope().LinkVar<VAR_T>(name, var, desc, default_val);
+    }
+
+    /// Link a configuration entry to a pair of functions - it sets the new default and
+    /// automatically calls the set function when configs are loaded.
+    template <typename VAR_T, typename DEFAULT_T>
+    ConfigEntry_Functions<VAR_T> & LinkFuns(std::function<VAR_T()> get_fun,
+                                            std::function<void(const VAR_T &)> set_fun,
+                                            const std::string & name,
+                                            const std::string & desc,
+                                            DEFAULT_T default_val) {
+      return GetScope().LinkFuns<VAR_T>(name, get_fun, set_fun, desc, default_val);
+    }
+
   public:
     virtual void SetupScope(ConfigScope & scope) { cur_scope = &scope; }
     virtual void SetupConfig() = 0;
