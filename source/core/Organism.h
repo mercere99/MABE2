72e8c6594f09ef0fbcceb66881c0f29583556328
Charles Ofria <charles.ofria@gmail.com>
2019-09-26 17:27:19 -0400

    Renamed organism manager_ptr to just manager; made non-const; setup congig linker to forward.

--- source/core/Organism.h
+++ source/core/Organism.h
@@ -22,15 +22,35 @@ namespace mabe {
 
   class Organism {
   protected:
-    emp::VarMap var_map;                    ///< Map of all dynamic variables assigned to organism
-    emp::Ptr<const OrganismManager> manager_ptr;  ///< Pointer the the specific organism type
+    emp::VarMap var_map;                          ///< Dynamic variables assigned to organism
+    emp::Ptr<OrganismManager> manager;  ///< Pointer the the specific organism type
+
+    // Helper functions.
+    ConfigScope & GetScope() { return manager->GetScope(); }
+
+  protected:
+    // Forward all variable linkage to the organism's manager.
+    template <typename... Ts>
+    auto & LinkVar(Ts &&... args) { return manager->LinkVar(args...); }
+
+    template <typename VAR_T, typename DEFAULT_T>
+    auto & LinkFuns(std::function<VAR_T()> get_fun,
+                    std::function<void(const VAR_T &)> set_fun,
+                    const std::string & name,
+                    const std::string & desc,
+                    DEFAULT_T default_val) {
+      return manager->LinkFuns<VAR_T, DEFAULT_T>(get_fun, set_fun, name, desc, default_val);
+    }
+
+    // template <typename... Ts>
+    // auto & LinkPop(Ts &&... args) { return manager->LinkPop(args...); }
 
   public:
-    Organism(emp::Ptr<const OrganismManager> _ptr) : manager_ptr(_ptr) { ; }
+    Organism(emp::Ptr<OrganismManager> _ptr) : manager(_ptr) { ; }
     virtual ~Organism() { ; }
 
-    const OrganismManager & GetManager() { emp_assert(manager_ptr); return *manager_ptr; }
-    const OrganismManager & GetManager() const { emp_assert(manager_ptr); return *manager_ptr; }
+    const OrganismManager & GetManager() { emp_assert(manager); return *manager; }
+    const OrganismManager & GetManager() const { emp_assert(manager); return *manager; }
 
     bool HasVar(const std::string & name) const { return var_map.Has(name); }
     template <typename T> T & GetVar(const std::string & name) { return var_map.Get<T>(name); }
@@ -48,18 +68,18 @@ namespace mabe {
 
     /// We MUST be able to make a copy of organisms for MABE to function.  If this function
     /// is not overridden, try to the equivilent function in the organism manager.
-    virtual emp::Ptr<Organism> Clone() const { return manager_ptr->CloneOrganism(*this); }
+    virtual emp::Ptr<Organism> Clone() const { return manager->CloneOrganism(*this); }
 
     /// If we are going to print organisms (to screen or file) we need to be able to convert
     /// them to strings.  If this function is not overridden, try to the equivilent function
     /// in the organism manager.
-    virtual std::string ToString() { return manager_ptr->ToString(*this); }
+    virtual std::string ToString() { return manager->ToString(*this); }
 
     /// For evolution to function, we need to be able to mutate offspring.
-    virtual size_t Mutate(emp::Random & random) { return manager_ptr->Mutate(*this, random); }
+    virtual size_t Mutate(emp::Random & random) { return manager->Mutate(*this, random); }
 
     /// Completely randomize a new organism (typically for initialization)
-    virtual void Randomize(emp::Random & random) { manager_ptr->Randomize(*this, random); }
+    virtual void Randomize(emp::Random & random) { manager->Randomize(*this, random); }
 
     /// Generate an output and place it in the VarMap under the provided name (default = "result").
     /// Arguments are the output name int he VarMap and the output ID.
@@ -73,7 +93,7 @@ namespace mabe {
     /// --- Extra functions for when this is used a a prototype organism ---
     
     /// Setup organism-specific configuration options.
-    virtual void SetupConfig(MABE &) { ; }
+    virtual void SetupConfig() { ; }
 
   };
 
