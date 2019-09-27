a766605b898eb70d5acfcbc5ddb50e079198bc2f
Charles Ofria <charles.ofria@gmail.com>
2019-09-27 00:03:49 -0400

    Added a base class for organism managers.

---
+++ source/core/OrganismManagerBase.h
@@ -0,0 +1,74 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019
+ *
+ *  @file  OrganismManagerBase.h
+ *  @brief Base class for tracking a category of organism.
+ */
+
+#ifndef MABE_ORGANISM_MANAGER_BASE_H
+#define MABE_ORGANISM_MANAGER_BASE_H
+
+#include <functional>
+#include <iostream>
+
+#include "base/unordered_map.h"
+#include "data/VarMap.h"
+#include "tools/Random.h"
+
+#include "../config/Config.h"
+
+namespace mabe {
+
+  class Organism;
+  class MABE;
+
+  class OrganismManagerBase  : public mabe::ConfigType {
+  protected:
+    std::string name;              ///< Name used for this type of organisms.
+    emp::VarMap var_map;           ///< Map of run-time values associated with this organism type.
+
+    emp::Ptr<Organism> prototype;  ///< Base organism to copy.
+
+  public:
+    OrganismManagerBase(const std::string & in_name) : name(in_name) { ; }
+    virtual ~OrganismManagerBase() { ; }
+
+    const std::string & GetName() const { return name; }
+    virtual std::string GetTypeName() const { return "OrganismManagerBase (base)"; }
+
+    // --== Functions to manipulate config variables ==--
+    template <typename T>
+    OrganismManagerBase & AddVar(const std::string & name, const std::string & desc, const T & def_val) {
+      var_map.Add<T>(name, def_val);
+      return *this;
+    }
+
+    template <typename T>
+    const T & GetVar(const std::string & name) {
+      return var_map.Get<T>(name);
+    }
+
+    // --== Functions to manipulate organisms ==--
+    /// Create a clone of the provided organism.
+    virtual emp::Ptr<Organism> CloneOrganism(const Organism &) const = 0;
+    virtual emp::Ptr<Organism> MakeOrganism() const = 0;
+    virtual emp::Ptr<Organism> MakeOrganism(emp::Random &) const = 0;
+    virtual std::string ToString(const Organism &) const = 0;
+    virtual std::ostream & Print(Organism &, std::ostream &) const = 0;
+
+    virtual size_t Mutate(Organism &, emp::Random &) const {
+      emp_assert(false, "Mutate() must be overridden for either Organism or OrganismManagerBase.");
+      return 0;
+    }
+    virtual void Randomize(Organism &, emp::Random &) const {
+      emp_assert(false, "Randomize() must be overridden for either Organism or OrganismManagerBase.");
+    }
+
+    virtual void SetupConfig() { }
+  };
+
+}
+
+#endif
