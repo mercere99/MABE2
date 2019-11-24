0a382bf53d67fa5614057a32c5030f86d0aa092a
Charles Ofria <charles.ofria@gmail.com>
2019-11-23 23:36:55 -0500

    Commiting OrganismManager_Wrapper.h with deletions before file removal.

--- source/core/OrganismManager_Wrapper.h
+++ source/core/OrganismManager_Wrapper.h
@@ -27,58 +27,11 @@ namespace mabe {
   public:
     using org_t = ORG_T;
     using this_t = OrganismManager_Wrapper<ORG_T>;
-    using OrganismManager<ORG_T>::prototype;
 
     OrganismManager_Wrapper(const std::string & name)
     : OrganismManager<ORG_T>(name) { ; }
 
     ~OrganismManager_Wrapper() {
-    }
-
-    /// Convert this organism to the correct type (after ensuring that it is!)
-    org_t & ConvertOrg(Organism & org) const {
-      emp_assert(&(org.GetManager()) == this);
-      return (org_t &) org;
-    }
-
-    /// Convert this CONST organism to the correct type (after ensuring that it is!)
-    const org_t & ConvertOrg(const Organism & org) const {
-      emp_assert(&(org.GetManager()) == this);
-      return (org_t &) org;
-    }
-
-    /// Create a clone of the provided organism; default to using copy constructor.
-    emp::Ptr<Organism> CloneOrganism(const Organism & org) const override {
-      return emp::NewPtr<org_t>( ConvertOrg(org) );
-    }
-
-    /// Create a random organism from scratch.  Default to using the prototype organism.
-    emp::Ptr<Organism> MakeOrganism() const override {
-      auto org_ptr = prototype->Clone();
-      return org_ptr;
-    }
-
-    /// Create a random organism from scratch.  Default to using the prototype organism
-    /// and then randomize if a random number generator is provided.
-    emp::Ptr<Organism> MakeOrganism(emp::Random & random) const override {
-      auto org_ptr = prototype->Clone();
-      org_ptr->Randomize(random);
-      return org_ptr;
-    }
-
-    /// Convert an organism to a string for printing; if not overridden, just prints
-    /// "__unknown__".
-    std::string ToString(const Organism &) const override { return "__unknown__"; };
-
-    /// By default print an organism by triggering it's ToString() function.
-    std::ostream & Print(Organism & org, std::ostream & os) const override {
-      emp_assert(&(org.GetManager()) == this);
-      os << org.ToString();
-      return os;
-    }
-
-    void SetupConfig() override {
-      prototype->SetupConfig();
     }
 
   };
