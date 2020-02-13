63b303875c4454e62c816d2da30fd70e1bfbfc62
Charles Ofria <charles.ofria@gmail.com>
2020-02-12 22:02:17 -0500

    Setup EmptyOrganismManager to take its MABE controller as an argument.

--- source/core/EmptyOrganism.h
+++ source/core/EmptyOrganism.h
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2019
+ *  @date 2019-2020.
  *
  *  @file  EmptyOrganism.h
  *  @brief Simple organism placeholder to represent open positions in a population.
@@ -11,14 +11,17 @@
 #define MABE_EMPTY_ORGANISM_H
 
 #include "Organism.h"
+#include "ModuleBase.h"
 
 namespace mabe {
 
   class EmptyOrganism;
 
-  class EmptyOrganismManager : public OrganismManagerBase {
+  class EmptyOrganismManager : public ModuleBase {
   public:
-    EmptyOrganismManager() : OrganismManagerBase("Empty Organism Manager") { ; }
+    EmptyOrganismManager(MABE & in_control)
+      : ModuleBase(in_control, "EmptyOrganismManager"
+      , "Manager for all 'empty' organisms in any population.") { ; }
     ~EmptyOrganismManager() { ; }
 
     std::string GetTypeName() const override { return "EmptyOrganismManager"; }
@@ -27,8 +30,8 @@ namespace mabe {
     emp::Ptr<Organism> CloneOrganism(const Organism &) const override { emp_error("Do not call functions on EmptyOrganism."); return nullptr; }
     emp::Ptr<Organism> MakeOrganism() const override { emp_error("Do not call functions on EmptyOrganism."); return nullptr; }
     emp::Ptr<Organism> MakeOrganism(emp::Random &) const override { emp_error("Do not call functions on EmptyOrganism."); return nullptr; }
-    std::string ToString(const Organism &) const override { emp_error("Do not call functions on EmptyOrganism."); return ""; }
-    std::ostream & Print(Organism &, std::ostream & os) const override { emp_error("Do not call functions on EmptyOrganism."); return os; }
+    std::string OrgToString(const Organism &) const override { emp_error("Do not call functions on EmptyOrganism."); return ""; }
+    std::ostream & PrintOrganism(Organism &, std::ostream & os) const override { emp_error("Do not call functions on EmptyOrganism."); return os; }
   };
 
   static EmptyOrganismManager & GetEmptyOrgManager() {
@@ -40,11 +43,11 @@ namespace mabe {
   class EmptyOrganism : public Organism {
   public:
     EmptyOrganism() : Organism(GetEmptyOrgManager()) { ; }
-    emp::Ptr<Organism> Clone() const override { emp_assert(false, "Do not clone EmptyOrganism"); return nullptr; }
+    emp::Ptr<Organism> Clone() const override { emp_error("Do not clone EmptyOrganism"); return nullptr; }
     std::string ToString() override { return "[empty]"; }
-    size_t Mutate(emp::Random &) override { emp_assert(false, "EmptyOrganism cannot Mutate()"); return -1; }
-    void Randomize(emp::Random &) override { emp_assert(false, "EmptyOrganism cannot Randomize()"); }
-    void GenerateOutput(const std::string &, size_t) override { emp_assert(false, "EmptyOrganism cannot GenerateOutput()"); }
+    size_t Mutate(emp::Random &) override { emp_error("EmptyOrganism cannot Mutate()"); return -1; }
+    void Randomize(emp::Random &) override { emp_error("EmptyOrganism cannot Randomize()"); }
+    void GenerateOutput(const std::string &, size_t) override { emp_error("EmptyOrganism cannot GenerateOutput()"); }
     emp::TypeID GetOutputType(size_t=0) override { return emp::TypeID(); }
     bool IsEmpty() const noexcept override { return true; }
   };
