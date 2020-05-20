9a80e2e2ff2a2298376bda676e1ac3828f75c57e
Charles Ofria <charles.ofria@gmail.com>
2020-05-20 01:02:36 -0400

    Setup CommandLine module to print some basic information before exiting.

--- source/interface/CommandLine.h
+++ source/interface/CommandLine.h
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2019
+ *  @date 2019-2020.
  *
  *  @file  CommandLine.h
  *  @brief Module to output errors and warnings to the command line.
@@ -42,6 +42,17 @@ namespace mabe {
       for (size_t pop_id = 0; pop_id < control.GetNumPopulations(); pop_id++) {
         const Population & pop = control.GetPopulation(pop_id);
         std::cout << "  " << pop.GetName() << ":" << pop.GetNumOrgs();
+      }
+      std::cout << std::endl;
+    }
+
+    void BeforeExit() override {
+      auto & pop = control.GetPopulation(pop_id);
+      std::cout << "Exiting.  Population " << pop.GetName()
+                << " has " << pop.GetNumOrgs() << " organisms.";
+      if (pop.GetNumOrgs()) {
+        auto & org = *(pop.Alive().begin());
+        std::cout << "  First org:\n" << org.ToString();
       }
       std::cout << std::endl;
     }
