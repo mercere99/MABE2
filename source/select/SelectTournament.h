217a579aeeac34e6070001445821e0c8010e77cb
Charles Ofria <charles.ofria@gmail.com>
2020-05-23 08:40:58 -0400

    Quite update of dates on several files.

--- source/select/SelectTournament.h
+++ source/select/SelectTournament.h
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2019
+ *  @date 2019-2020.
  *
  *  @file  SelectTournament.h
  *  @brief MABE module to enable tournament selection (choose T random orgs and return "best")
