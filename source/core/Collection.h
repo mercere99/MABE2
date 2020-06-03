878223871ba6b97b293ced3ea986c095f820dcb6
Charles Ofria <charles.ofria@gmail.com>
2020-06-03 00:52:39 -0400

    Cleanup methods for adding to this collection and trimming it down.

--- source/core/Collection.h
+++ source/core/Collection.h
@@ -24,30 +24,52 @@
 
 namespace mabe {
 
-  class Collection {
-  private:
-    std::set<OrgPosition> orgs;
-
+  class Collection : public std::set<OrgPosition> {
   public:
     Collection() = default;
     Collection(const Collection &) = default;
     Collection(Collection &&) = default;
+    Collection(const Population & pop) { Insert(pop); }
     ~Collection() { }
 
     Collection & operator=(const Collection &) = default;
     Collection & operator=(Collection &&) = default;
 
-    size_t GetSize() const { return orgs.size(); }
+    using iterator = std::set<OrgPosition>::iterator;;
+
+    size_t GetSize() const { return size(); }
 
     /// Add a Population to this collection.
-    void AddPop(Population & pop) {
+    Collection &  Insert(const Population & pop) {
       for (size_t i = 0; i < pop.GetSize(); i++) {
-        orgs.insert(OrgPosition(&pop, i));
+        insert(OrgPosition(&pop, i));
       }
+      return *this;
     }
 
     /// Add an organism (by position!)
-    void AddOrg(OrgPosition pos) { orgs.insert(pos); }
+    Collection & Insert(OrgPosition pos) { insert(pos); return *this; }
+
+    /// Add a whole other collection.
+    Collection & Insert(const Collection & collection) {
+      for (auto pos : collection) insert(pos);
+      return *this;
+    }
+
+    /// Merge this collection with another collection.
+    Collection & operator |= (const Collection & collection2) { return Insert(collection2); }
+
+    /// Reduce to the intersection with another colleciton.
+    Collection & operator &= (const Collection & collection2) {
+      auto it1 = begin();
+      auto it2 = collection2.begin();
+      while (it1 != end() && it2 != collection2.end()) {
+        if (it1 < it2) it1 = erase(it1);  // Not in collection2!
+        else if (it2 < it1) it2++;        // Need to catch up iterator for collection2.
+        else { it1++; it2++; }            // In both.  Keep and move on.
+      }
+    }
+
   };
 
 }
