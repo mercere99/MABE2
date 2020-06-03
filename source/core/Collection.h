a878b9962e5de2941d853481bf70c12d04a0ee29
Charles Ofria <charles.ofria@gmail.com>
2020-06-02 23:09:51 -0400

    Added stub for Collections or organisms.

---
+++ source/core/Collection.h
@@ -0,0 +1,32 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2020.
+ *
+ *  @file  Collection.h
+ *  @brief A collection of organisms or whole populations; not owner.
+ *
+ *  While organisms must be managed by Population objects, collections are an easy way
+ *  to represent and manipulate groups of organisms, either as whole populations or as
+ *  sets of individuals.
+ */
+
+#ifndef MABE_COLLECTION_H
+#define MABE_COLLECTION_H
+
+#include <string>
+
+#include "base/Ptr.h"
+#include "base/vector.h"
+
+#include "Population.h"
+
+namespace mabe {
+
+  class Collection {
+
+  };
+
+}
+
+#endif
