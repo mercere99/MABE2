131304d5b58b27ab28738adee69bf6f5ce2f2c05
Charles Ofria <charles.ofria@gmail.com>
2020-10-24 16:31:29 -0400

    Added Collection::size() for STL compatibility.

--- source/core/Collection.h
+++ source/core/Collection.h
@@ -207,6 +207,7 @@ namespace mabe {
       }
       return count;
     }
+    inline size_t size() { return GetSize(); }  // Alias for STL compatibility.
 
     Organism & At(size_t org_id) override {
       for (auto [pop_ptr, pop_info] : pos_map) {
