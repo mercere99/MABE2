37e432c060bc211065c0fc8f16a800f5b03ba66b
Austin Ferguson <ferguson.austin.j@gmail.com>
2020-10-27 21:23:59 -0400

    Clean code and comments in EvalTaskNOT

--- source/evaluate/EvalTaskNot.h
+++ source/evaluate/EvalTaskNot.h
@@ -4,7 +4,7 @@
  *  @date 2019-2020.
  *
  *  @file  EvalTaskNot.h
- *  @brief This is JUST A TEST (should be deleted later) to see test task eval via triggers
+ *  @brief Tests organism output for NOT operation on a ManualEval trigger
  */
 
 #ifndef MABE_EVAL_TASK_NOT_H
@@ -13,54 +13,48 @@
 #include "../core/MABE.h"
 #include "../core/Module.h"
 
-#include "tools/reference_vector.h"
-
 namespace mabe {
 
   class EvalTaskNot : public Module {
   private:
-    Collection target_collect;
-    std::string inputs_trait;
-    std::string outputs_trait;
-    std::string fitness_trait;
+    std::string inputs_trait;  ///< Name of trait for organism's inputs  (required)
+    std::string outputs_trait; ///< Name of trait for organism's outputs (required)
+    std::string fitness_trait; ///< Name of trait for organism's fitness (owned)
 
   public:
     EvalTaskNot(mabe::MABE & control,
                   const std::string & name="EvalTaskNot",
-                  const std::string & desc="{TEST} Evaluate organism on NOT via triggers",
+                  const std::string & desc="Evaluate organism on NOT via triggers",
                   const std::string & _itrait="inputs",
                   const std::string & _otrait="outputs",
                   const std::string & _ftrait="fitness")
       : Module(control, name, desc)
-      , target_collect(control.GetPopulation(0))
       , inputs_trait(_itrait)
       , outputs_trait(_otrait)
       , fitness_trait(_ftrait)
-    {
-      SetEvaluateMod(true);
-    }
+    { }
     ~EvalTaskNot() { }
 
     void SetupConfig() override {
-      LinkCollection(target_collect, "target", "Which population(s) should we evaluate?");
-      LinkVar(outputs_trait, "inputs_trait", "Which trait containts the organism's inputs?");
-      LinkVar(outputs_trait, "outputs_trait", "Which trait containts the organism's outputs?");
+      LinkVar(outputs_trait, "inputs_trait", "Which trait contains the organism's inputs?");
+      LinkVar(outputs_trait, "outputs_trait", "Which trait contains the organism's outputs?");
       LinkVar(fitness_trait, "fitness_trait", "Which trait should we store NK fitness in?");
     }
 
     void SetupModule() override {
       AddRequiredTrait<std::unordered_map<int,double>>(inputs_trait);
       AddRequiredTrait<std::unordered_map<int,double>>(outputs_trait);
-      AddOwnedTrait<double>(fitness_trait, "All-ones fitness value", 1.0);
+      AddOwnedTrait<double>(fitness_trait, "Organism's fitness value", 1.0);
     }
 
+    /// Test to see if organism performed the NOT operation
     void OnManualEval(Organism& org) override {
-        // Count the number of ones in the bit sequence.
         const std::unordered_map<int,double> & inputs = 
             org.GetVar<std::unordered_map<int,double>>(inputs_trait);
         const std::unordered_map<int,double> & outputs = 
             org.GetVar<std::unordered_map<int,double>>(outputs_trait);
         if(inputs.size() > 0 && outputs.size() > 0){
+          // TODO: Modify AvidaGPOrg so that it's not using doubles everywhere
           const uint64_t input = static_cast<uint64_t>(inputs.begin()->second);
           const uint64_t output = static_cast<uint64_t>(outputs.begin()->second);
           if(output == ~input){
@@ -76,7 +70,8 @@ namespace mabe {
     }
   };
 
-  MABE_REGISTER_MODULE(EvalTaskNot, "Test module for evaluating tasks via triggers");
+  MABE_REGISTER_MODULE(EvalTaskNot, "Manual evaluation of organism for NOT operation");
+
 }
 
 #endif
