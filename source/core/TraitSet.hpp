7dc282e51e72f5a1f45957a71e26769a6632e1c1
Charles Ofria <charles.ofria@gmail.com>
2021-03-30 00:18:01 -0400

    Added various types of counting of traits and values to TraitSet.

--- source/core/TraitSet.hpp
+++ source/core/TraitSet.hpp
@@ -7,6 +7,8 @@
  *  @brief A collection of traits with the same type (or collections of that type).
  *
  *  A TraitSet is used to keep track of a collection of related traits in a module.
+ * 
+ *  @CAO: Should this class be moved into Empirical proper?
  */
 
 #ifndef MABE_TRAIT_SET_H
@@ -31,6 +33,7 @@ namespace mabe {
 
     const emp::DataLayout & layout;
 
+    size_t num_values = 0;
     std::string error_trait = "";
   public:
     TraitSet(const emp::DataLayout & in_layout) : layout(in_layout) { }
@@ -39,6 +42,7 @@ namespace mabe {
     void Clear() {
       base_names.resize(0); vector_names.resize(0);
       base_IDs.resize(0); vector_IDs.resize(0);
+      num_values = 0;
     }
 
     /// Add any number of traits, separated by commas.
@@ -70,6 +74,33 @@ namespace mabe {
     template <typename... Ts>
     bool AddTraits(const std::string & in_names, const std::string & next_names, Ts &... extras) {
       return AddTraits(in_names) && AddTraits(next_names, extras...);
+    }
+
+    /// Clear any existing traits and load in the ones provided.
+    template <typename... Ts>
+    bool SetTraits(Ts &... traits) {
+      Clear();
+      return AddTraits(traits...);
+    }
+
+    /// Total number of direct traits.
+    size_t GetNumBaseTraits() const { return base_IDs.size(); }
+
+    /// Total number of traits that are collections of values in vectors.
+    size_t GetNumVectorTraits() const { return vector_IDs.size(); }
+
+    /// Get the total number of traits being monitored (regular values + vectors of values)
+    size_t GetNumTraits() const { 
+      return base_IDs.size() + vector_IDs.size();
+    }
+
+    /// Count the total number of individual values across all traits and store for future use.
+    size_t CountValues(const DataMap & dmap) const {
+      size_t count = base_IDs.size();
+      for (size_t id : vectorIDs) {
+        count += dmap.Get<emp::vector<T>>(id).size();
+      }
+      return count;
     }
   };
 
