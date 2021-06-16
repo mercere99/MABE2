241b2a777ccb91115c1b3e1110eb74a83fb3f560
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-06-16 15:52:56 -0400

    Add example test

---
+++ tests/component/core/MABE.cpp
@@ -0,0 +1,29 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019-2021.
+ *
+ *  @file  MABE.cpp
+ *  @brief Tests for the main MABE object 
+ */
+
+// CATCH
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// Empirical tools
+#include "emp/bits/BitVector.hpp"
+#include "emp/config/ArgManager.hpp"
+#include "emp/math/Random.hpp"
+// MABE
+#include "../../../source/core/MABE.hpp"
+#include "../../../source/core/EmptyOrganism.hpp"
+#include "../../../source/modules.hpp"
+
+
+TEST_CASE("MABE", "[core]"){
+  {
+    mabe::MABE control(0, NULL);
+    REQUIRE(control.GetUpdate() == 0);
+    //REQUIRE(1 + 1 == 2);
+  }
+}
