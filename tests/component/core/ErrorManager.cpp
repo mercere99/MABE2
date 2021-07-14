ee471324bd24daf13d865c44da30e96e195f79bb
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-07-14 15:20:34 -0700

    Add getter tests

--- tests/component/core/ErrorManager.cpp
+++ tests/component/core/ErrorManager.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <functional>
+#include <string>
 // CATCH
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
@@ -53,7 +54,7 @@ TEST_CASE("ErrorManagerBasic", "[core]"){
     
     REQUIRE(has_error_been_thrown == 0);
     REQUIRE(has_warning_been_thrown == 0);
-    /*
+
     // Flush manager to display queued errors and warnings
     manager.Flush();
     REQUIRE(has_error_been_thrown == 1);
@@ -62,14 +63,16 @@ TEST_CASE("ErrorManagerBasic", "[core]"){
     // Add errors and warnings to active manager, thrown immediately 
     has_error_been_thrown = false;
     has_warning_been_thrown = false;
+
     manager.Activate();
 
     manager.AddError("Error01");
     REQUIRE(has_error_been_thrown == 1);
+
     manager.AddWarning("Warning01");
     REQUIRE(has_warning_been_thrown == 1);
 
-    // Update error and warnign callback functions
+    // Update error and warning callback functions
     has_error_been_thrown = false;
     has_warning_been_thrown = false;
     bool new_error_cb = false;
@@ -92,18 +95,33 @@ TEST_CASE("ErrorManagerBasic", "[core]"){
     manager.SetWarningCallback(warning_callback_01);
 
     // Add errors and warnings to active manager, throw immediately
+    has_error_been_thrown = false;
+    has_warning_been_thrown = false;
+
     manager.Activate();
     manager.AddError("Error02");
     manager.AddWarning("Warning02");
 
     REQUIRE(has_error_been_thrown == 1);
     REQUIRE(new_error_cb == 1);
-    REQUIRE(has_warning_been_thrown == 0);
+    REQUIRE(has_warning_been_thrown == 1);
     REQUIRE(new_warning_cb == 1);
-    */
+
+    // Deactivate manager
+    has_error_been_thrown = false;
+    has_warning_been_thrown = false;
+
+    manager.Deactivate();
+    manager.AddError("Error03");
+    manager.AddWarning("Warning03");
+
+    REQUIRE(has_error_been_thrown == 0);
+    REQUIRE(has_warning_been_thrown == 0);
+
   }
 }
-/*
+
+
 TEST_CASE("ErrorManagerGetters", "[core]"){
   {
      // Error callback function
@@ -118,34 +136,49 @@ TEST_CASE("ErrorManagerGetters", "[core]"){
     };
 
     // Create error manager instance
-    mabe::ErrorManager manager(error_callback_00, warning_callback_00);
+    mabe::ErrorManager manager01(error_callback_00, warning_callback_00);
 
     //Getter defaults
-    REQUIRE(GetErrors() == "");
-    REQUIRE(GetWarnings() == "");
-    REQUIRE(NumErrors() == 0);
-    REQUIRE(NumWarnings() == 0);
-    REQUIRE(IsActive() == false);
+    REQUIRE(manager01.GetErrors().empty() == true);
+    REQUIRE(manager01.GetWarnings().empty() == true);
+    REQUIRE(manager01.GetNumErrors() == 0);
+    REQUIRE(manager01.GetNumWarnings() == 0);
+    REQUIRE(manager01.IsActive() == false);
 
-    manager.Activate();
-    REQUIRE(IsActive() == true);
+    manager01.Activate();
+    REQUIRE(manager01.IsActive() == true);
+
     // Error and Warnings getter functions
-    manager.AddError("Error00");
-    manager.AddWarnings("Warning00");
+    manager01.AddError("Error01");
+    manager01.AddWarning("Warning01");
 
-    REQUIRE(GetErrors() == "Error00");
-    REQUIRE(GetWarnings() == "Warning00");
+    std::string error01 = manager01.GetErrors().back();
+    REQUIRE(error01.compare("Error01") == 0);
+    std::string warning01 = manager01.GetWarnings().back();
+    REQUIRE(warning01.compare("Warning01") == 0);
 
     //NumError and NumWarning getter functions
-    REQUIRE(NumErrors() == 1);
-    REQUIRE(NumWarnings() == 1);
+    REQUIRE(manager01.GetNumErrors() == 1);
+    REQUIRE(manager01.GetNumWarnings() == 1);
 
-    manager.AddError("Error01");
-    manager.AddWarning("Warning01");
+    // Getters with multiple errors and warnings
+    manager01.AddError("Error02");
+    manager01.AddWarning("Warning02");
 
-    REQUIRE(GetErrors() == "Error01 Error00");
-    REQUIRE(GetWarnings() == "Warning01 Warning00");
-    REQUIRE(NumErrors() == 2);
-    REQUIRE(NumWarnings() == 2);
+    std::string error02 = manager01.GetErrors().back();
+    REQUIRE(error02.compare("Error02") == 0);
+    std::string warning02 = manager01.GetWarnings().back();
+    REQUIRE(warning02.compare("Warning02") == 0);
+    REQUIRE(manager01.GetNumErrors() == 2);
+    REQUIRE(manager01.GetNumWarnings() == 2);
+
+    // Clear manager
+    manager01.Clear();
+
+    REQUIRE(manager01.GetErrors().empty() == true);
+    REQUIRE(manager01.GetWarnings().empty() == true);
+    REQUIRE(manager01.GetNumErrors() == 0);
+    REQUIRE(manager01.GetNumWarnings() == 0);
+
+  }
 }
-*/
