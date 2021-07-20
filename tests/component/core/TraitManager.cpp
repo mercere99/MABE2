3983ad216806136e1261b422e8d96b4085061c3a
lanearohan <lanearohan@gmail.com>
2021-07-20 11:29:00 -0500

    Setup files for TraitManger testfile to run

--- tests/component/core/TraitManager.cpp
+++ tests/component/core/TraitManager.cpp
@@ -13,30 +13,39 @@
 // MABE
 #include "core/TraitInfo.hpp"
 #include "core/TraitManager.hpp"
+#include "core/ErrorManager.hpp"
 #include "core/MABE.hpp"
-#include "evaluate/static/EvalNK.hpp"
+//#include "evaluate/static/EvalNK.hpp"
 
 TEST_CASE("TraitManager_Basic", "[core]") {
     {
         //hello
+        REQUIRE(0 == 0); 
     }
 }
 
 TEST_CASE("TraitManager_LockMethods", "[core]") {
     {
-        //hello
+        // Create a TraitManager
+        //mabe::TraitManager trait_man;
+
+        REQUIRE(0 == 0); 
+
+
     }
 }
 
 TEST_CASE("TraitManager_AccessSpecifications", "[core]") {
     {
         //hello
+        REQUIRE(0 == 0); 
     }
 }
 
 TEST_CASE("TraitManager_AddTrait", "[core]") {
     {
         //hello
+        REQUIRE(0 == 0); 
     }
 }
 
