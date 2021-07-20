7716b80f197e4126fa0f8425691defea225b6d97
lanearohan <lanearohan@gmail.com>
2021-07-20 10:34:11 -0500

    Add header and includes

---
+++ tests/component/core/TraitManager.cpp
@@ -0,0 +1,20 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019-2021.
+ *
+ *  @file  TraitManager.cpp
+ *  @brief Tests for the TraitManager file
+ */
+
+// CATCH
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// MABE
+#include "core/TraitInfo.hpp"
+#include "core/TraitManager.hpp"
+#include "core/MABE.hpp"
+#include "evaluate/static/EvalNK.hpp"
+
+
+
