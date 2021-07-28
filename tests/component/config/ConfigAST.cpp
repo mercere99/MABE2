f43f79663651cb4251dc4829f87b2b22e505ef21
AriaKillebrewBruehl <ariadkb@gmail.com>
2021-07-28 07:43:40 -0700

    Add tests for Math1 and Math2

--- tests/component/config/ConfigAST.cpp
+++ tests/component/config/ConfigAST.cpp
@@ -8,6 +8,8 @@
  */
 
 #include <iostream>
+#include <functional>
+
 // CATCH
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
@@ -61,35 +63,31 @@ TEST_CASE("ASTNode_Block", "[config]"){
     REQUIRE(str00.compare("") == 0);
 
     REQUIRE(block00.GetNumChildren() == 0);
-    //REQUIRE_THROWS(block00.GetChild(0));
 
     // Test boolean functions
     REQUIRE(block00.IsInternal());
 
     // Test adding children
-    /*int v00 = 0;
-    mabe::ConfigEntry_Linked entry00("name00", v00, "variable00", nullptr);
+    int v00 = 0;
+    mabe::ConfigEntry_Linked<int> entry00("name00", v00, "variable00", nullptr);
 
-    entry_ptr_t ptr00 = &entry00;
-    mabe::ASTNode_Leaf leaf00(ptr00);
-    node_ptr_t child00 = &leaf00;
-    block00.AddChild(child00);
+    emp::Ptr<mabe::ASTNode_Leaf> leaf00 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry00);
+    block00.AddChild(leaf00);
 
     REQUIRE(block00.GetNumChildren() == 1);
-    REQUIRE(&block00.GetChild(0) == child00.GetEntry().Raw());
+    REQUIRE(block00.GetChild(0)->IsLeaf());
+    REQUIRE(block00.GetChild(0)->Process() == leaf00->Process());
 
     // Test multiple children
     int v01 = 1;
     mabe::ConfigEntry_Linked entry01("name01", v01, "variable01", nullptr);
 
-    entry_ptr_t ptr01 = &entry01;
-    mabe::ASTNode_Leaf leaf01(ptr01);
-    node_ptr_t child01 = &leaf01;
-    block00.AddChild(child01);
+    emp::Ptr<mabe::ASTNode_Leaf> leaf01 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry01);
+    block00.AddChild(leaf01);
 
     REQUIRE(block00.GetNumChildren() == 2);
-
-    REQUIRE(block00.GetChild(0) == child01);
+    REQUIRE(block00.GetChild(1)->IsLeaf());
+    REQUIRE(block00.GetChild(1)->Process() == leaf01->Process());
     
 
     // Test Process()
@@ -98,9 +96,7 @@ TEST_CASE("ASTNode_Block", "[config]"){
     // Test Write()
     std::stringstream ss;
     block00.Write(ss, "");
-    std::cout << "ss:" << ss.str() << "done";
-    REQUIRE(ss.str().compare("") == 0);
-    */
+    REQUIRE(ss.str().compare("name00;\nname01;\n") == 0);
   }
 }
 
@@ -110,63 +106,122 @@ double abs_value(double n) {
   }
   return n;
 }
-
 TEST_CASE("ASTNode_Math1", "[config]"){
   {
-    mabe::ASTNode_Math1 math100("name00");
+    mabe::ASTNode_Math1 math100("math00");
 
     // Test getters
     std::string str00 = math100.GetName();
-    REQUIRE(str00.compare("name00") == 0);
+    REQUIRE(str00.compare("math00") == 0);
 
     REQUIRE(math100.GetNumChildren() == 0);
-    REQUIRE(math100.GetChild(0) == nullptr);
 
     // Test adding children
     int v00 = -1;
-    mabe::ConfigEntry_Linked entry00("name00", v00, "variable00", nullptr);
+    mabe::ConfigEntry_Linked<int> entry00("name00", v00, "variable00", nullptr);
 
-    entry_ptr_t ptr00 = &entry00;
-    mabe::ASTNode_Leaf leaf00(ptr00);
-    node_ptr_t child00 = &leaf00;
-    block00.AddChild(child00);
+    emp::Ptr<mabe::ASTNode_Leaf> leaf00 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry00);
+    math100.AddChild(leaf00);
 
-    REQUIRE(block00.GetNumChildren() == 1);
-    REQUIRE(block00.GetChild(0) == child00);
+    REQUIRE(math100.GetNumChildren() == 1);
+    REQUIRE(math100.GetChild(0)->IsLeaf());
+    REQUIRE(math100.GetChild(0)->Process() == leaf00->Process());
 
     // Test boolean functions
     REQUIRE(math100.IsInternal());
 
     // Test setters
     math100.SetFun(abs_value);
+    
+    // Test Process() with one child
+    REQUIRE(math100.Process()->AsDouble() == 1);
 
-    //void AddChild(node_ptr_t child) { children.push_back(child); }
+    // Test Write()
+    std::stringstream ss;
+    math100.Write(ss, "");
+    std::cout << ss.str() << std::endl;
+    REQUIRE(ss.str().compare("math00name00") == 0);
 
-    // for process, test w more than one child, require throw
+    // Test adding multiple children
+    int v01 = -2;
+    mabe::ConfigEntry_Linked entry01("name01", v01, "variable01", nullptr);
+
+    emp::Ptr<mabe::ASTNode_Leaf> leaf01 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry01);
+    math100.AddChild(leaf01);
+    /*
+    bool process_completed = false;
+    std::function<void(mabe::ASTNode_Math1 &)> run_process = [&process_completed](mabe::ASTNode_Math1 & math){
+      math.Process();
+      process_completed = true;
+    };
+
+    REQUIRE(math100.GetNumChildren() == 2);
+    REQUIRE(math100.GetChild(1)->IsLeaf());
+    run_process(math100);
+    REQUIRE(process_completed == false);
+    */
+    // for process, test w more than one child, how do i test this?
   }
 }
 
-/*
+double add_fun(double n, double m) {
+  return n + m;
+}
 TEST_CASE("ASTNode_Math2", "[config]"){
   {
-    mabe::ASTNode_Math2 math200("name00");
+    mabe::ASTNode_Math2 math200("math00");
 
     // Test getters
     std::string str00 = math200.GetName();
-    REQUIRE(str00.compare("name00") == 0);
+    REQUIRE(str00.compare("math00") == 0);
 
     REQUIRE(math200.GetNumChildren() == 0);
 
     // Test boolean functions
     REQUIRE(math200.IsInternal());
 
-    size_t GetNumChildren() const override { return children.size(); }
-    node_ptr_t GetChild(size_t id) override { return children[id]; }
+    // Test adding children
+    int v00 = 1;
+    mabe::ConfigEntry_Linked<int> entry00("name00", v00, "variable00", nullptr);
 
-    void AddChild(node_ptr_t child) { children.push_back(child); }
+    emp::Ptr<mabe::ASTNode_Leaf> leaf00 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry00);
+    math200.AddChild(leaf00);
+
+    REQUIRE(math200.GetNumChildren() == 1);
+    REQUIRE(math200.GetChild(0)->IsLeaf());
+    REQUIRE(math200.GetChild(0)->Process() == leaf00->Process());
+
+    // Test Process() with only one child
+    // how do I do this?
+
+    // Add second child
+    int v01 = 2;
+    mabe::ConfigEntry_Linked<int> entry01("name01", v01, "variable01", nullptr);
+
+    emp::Ptr<mabe::ASTNode_Leaf> leaf01 = emp::NewPtr<mabe::ASTNode_Leaf>(&entry01);
+    math200.AddChild(leaf01);
+
+    REQUIRE(math200.GetNumChildren() == 2);
+    REQUIRE(math200.GetChild(1)->IsLeaf());
+    REQUIRE(math200.GetChild(1)->Process() == leaf01->Process());
+
+    // Set function
+    math200.SetFun(add_fun);
+
+    // Test Process()
+    REQUIRE(math200.Process()->AsDouble() == 3);
+
+    // Test Write() is this what it should be ?
+    std::stringstream ss;
+    math200.Write(ss, "");
+    std::cout << ss.str() << std::endl;
+    REQUIRE(ss.str().compare("math00name00name01") == 0);
+
+
+
   }
 }
-
+/*
 TEST_CASE("ASTNode_Assign", "[config]"){
   {
     mabe::ASTNode_Assign assign00("name00");
