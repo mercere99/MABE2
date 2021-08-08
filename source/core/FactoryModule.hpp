102e8e64fc16e4b04bf7352fa0edf39349370bbf
Charles Ofria <charles.ofria@gmail.com>
2021-08-08 16:40:15 -0400

    Cleaned up FactoryModule so it can compile.

--- source/core/FactoryModule.hpp
+++ source/core/FactoryModule.hpp
@@ -19,21 +19,49 @@
 
 namespace mabe {
 
+  // Pre-declarations...
   class MABE;
+  template <typename OBJ_T, typename BASE_T> class FactoryModule;
+
+  /// Base class for factory products that uses "curiously recursive templates" to fill
+  /// out default functionality for when you know the derived type.
+  template <typename OBJ_T, typename BASE_T>
+  class ProductTemplate : public BASE_T {
+  public:
+    ProductTemplate(ModuleBase & _man) : BASE_T(_man) { ; }
+
+    using obj_t = OBJ_T;
+    using manager_t = FactoryModule<OBJ_T, BASE_T>;
+
+    /// Get the manager for this type of organism.
+    manager_t & GetManager() {
+      return (manager_t &) BASE_T::GetManager();
+    }
+    const manager_t & GetManager() const {
+      return (const manager_t &) BASE_T::GetManager();
+    }
+
+    auto & SharedData() { return GetManager().data; }
+    const auto & SharedData() const { return GetManager().data; }
+  };
+
 
   /// @param OBJ_T the object type being managed by the factory.
   /// @param BASE_T the base object category being mnagaed by the factory.
   template <typename OBJ_T, typename BASE_T>
   class FactoryModule : public Module {
     /// Allow factory products to access private shared data in their own manager only.
-    friend ProductTemplate<OBJ_T>;
+    friend class ProductTemplate<OBJ_T, BASE_T>;
 
   private:
-    /// Locate the specification for the data that we need to store in the factory module.
-    using data_t = typename OBJ_T::ModuleData;
+    /// Locate the specification for the data that we need for management in the factory module.
+    using data_t = typename OBJ_T::ManagerData;
     
     /// Shared data across all objects that use this factory.
     data_t data;
+
+    /// Maintain a prototype for the objects being created.
+    emp::Ptr<BASE_T> obj_prototype;
 
   public:
     FactoryModule(MABE & in_control, const std::string & in_name, const std::string & in_desc="")
@@ -51,7 +79,7 @@ namespace mabe {
     emp::TypeID GetObjType() const override { return emp::GetTypeID<obj_t>(); }
 
     /// Create a clone of the provided object; default to using copy constructor.
-    emp::Ptr<BASE_T> Clone(const BASE_T & obj) override {
+    emp::Ptr<BASE_T> CloneObject(const BASE_T & obj) override {
       return emp::NewPtr<obj_t>( (const obj_t &) obj );
     }
 
@@ -98,40 +126,18 @@ namespace mabe {
     }
   };
 
-  /// Below is a base class for factory products that uses "curiously recursive templates" to fill
-  /// out default functionality for when you know the derived type.
-  template <typename OBJ_T, typename BASE_T>
-  class FactoryProductTemplate : public BASE_T {
-  public:
-    FactoryProductTemplate(ModuleBase & _man) : BASE_T(_man) { ; }
-
-    using obj_t = OBJ_T;
-    using manager_t = FactoryModule<OBJ_T, BASE_T>;
-
-    /// Get the manager for this type of organism.
-    manager_t & GetManager() {
-      return (manager_t &) BASE_T::GetManager();
-    }
-    const manager_t & GetManager() const {
-      return (const manager_t &) BASE_T::GetManager();
-    }
-
-    auto & SharedData() { return GetManager().data; }
-    const auto & SharedData() const { return GetManager().data; }
-  };
-
 
   /// MACRO for quickly adding new factory modules.
-  #define MABE_REGISTER_FACTORY_MODULE(TYPE, DESC) \
-        mabe::FactoryModuleRegistrar<FactoryModule<TYPE>> MABE_ ## TYPE ## _Registrar(#TYPE, DESC)
+  #define MABE_REGISTER_FACTORY_MODULE(TYPE, BASE_TYPE, DESC) \
+        mabe::FactoryModuleRegistrar<FactoryModule<TYPE, BASE_TYPE>> MABE_ ## TYPE ## _Registrar(#TYPE, DESC)
 
   // Setup backward compatability with OrganismManager.
   template <typename ORG_T>
   using OrganismManager = FactoryModule<ORG_T, mabe::Organism>;
   template <typename ORG_T>
-  using OrganismTemplate = FactoryProductTemplate<ORG_T, mabe::Organism>;
+  using OrganismTemplate = ProductTemplate<ORG_T, mabe::Organism>;
 
-  #define MABE_REGISTER_ORG_TYPE(TYPE, DESC) MABE_REGISTER_FACTORY_MODULE(TYPE, DESC)
+  #define MABE_REGISTER_ORG_TYPE(TYPE, DESC) MABE_REGISTER_FACTORY_MODULE(TYPE, mabe::Organism, DESC)
 
 }
 
