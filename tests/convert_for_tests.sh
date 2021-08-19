ac73d280c3e5823a1920a83e70138a92e1e7440d
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-08-19 16:23:58 -0400

    Try adding code coverage as action (again)

--- tests/convert_for_tests.sh
+++ tests/convert_for_tests.sh
@@ -2,16 +2,19 @@
 
 EMP_DIR=../source/third-party/empirical
 COV_DIR=./coverage_source
+TEST_DIR=./unit
+
+FLAGS="-std=c++17 -pthread -Wall -Wno-unused-function -Wno-unused-private-field -I${EMP_DIR}/include/ -I${EMP_DIR}/third-party/Catch/single_include/catch2 -I../source/ -DEMP_TRACK_MEM"
 
 mkdir ${COV_DIR} -p
 
 set -e # exit with error if any of this fails
 rm -rf ${COV_DIR} 
-cp -r ../source ${COV_DIR}
-for filename in $(find ${COV_DIR} -name "*.hpp" ! -name "_*" ! -path "*/OLD/*" ! -path "*/in_progress/*" ! -path "*/web/*" ! -path "*/third-party/*")
+cp -r ${TEST_DIR} ${COV_DIR}
+for filename in $(find ${COV_DIR} -name "*.cpp" ! -name "_*" ! -path "*/OLD/*" ! -path "*/in_progress/*" ! -path "*/web/*" ! -path "*/third-party/*")
 do
-    echo $filename
-    ${EMP_DIR}/third-party/force-cover/force_cover $filename -- -I. -I${COV_DIR} --language c++ -std=c++17 -DEMP_TRACK_MEM -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/x86_64-linux-gnu/c++/8 -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/../../../../include/c++/8/backward -I/usr/include/clang/7/include -I/usr/local/include -I/usr/bin/../lib/gcc/x86_64-linux-gnu/8/include -I/usr/include/x86_64-linux-gnu -I/usr/include -I${EMP_DIR}/include -I${EMP_DIR}/third-party/cereal/include/ | xargs -0 echo > $filename.temp
-    #mv $filename.temp $filename
+    echo "${filename}"
+    ${EMP_DIR}/third-party/force-cover/force_cover ${filename} -- -I. -I${COV_DIR} --language c++ ${FLAGS} | xargs -0 echo > ${filename}.temp
+    mv ${filename}.temp ${filename}
 
 done
