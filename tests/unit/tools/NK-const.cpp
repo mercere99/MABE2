09ef67072c87f34b6f9ced3739e301d30afbdc38
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-08-30 14:12:15 -0400

    Add placeholder tests for tools directory

---
+++ tests/unit/tools/NK-const.cpp
@@ -0,0 +1,17 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019-2021.
+ *
+ *  @file  NK-const.cpp
+ *  @brief TODO. Currently this is a placeholder so codecov will see the untested source code
+ */
+
+// CATCH
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// MABE
+#include "tools/NK-const.hpp"
+
+
+TEST_CASE("NK-const_Placeholder", "[core]"){ ; }
---
+++ tests/unit/tools/Resource.cpp
@@ -0,0 +1,17 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019-2021.
+ *
+ *  @file  Resource.cpp
+ *  @brief TODO. Currently this is a placeholder so codecov will see the untested source code
+ */
+
+// CATCH
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// MABE
+#include "tools/Resource.hpp"
+
+
+TEST_CASE("Resource_Placeholder", "[core]"){ ; }
