df8d9f3efdd6c6226e4c088ef4ae3b7a19e3c973
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-09-07 14:56:20 -0400

    Add actual value checking in regression tests

--- tests/regression/run_examples.sh
+++ tests/regression/run_examples.sh
@@ -4,8 +4,6 @@ SETTINGS_DIR="."
 MABE_FILES=`ls ${SETTINGS_DIR} | grep .mabe`
 THIS_DIR=`pwd`
 
-set -e # If any errors occur, propogate error and stop
-
 cd ${BUILD_DIR}
 make clean
 make debug
@@ -22,6 +20,23 @@ do
   cp ../../${SETTINGS_DIR}/${filename} ./
   ./MABE -f ${filename} > terminal_output.txt
 
+  # Check generated data against expected data
+  cmp terminal_output.txt ${THIS_DIR}/expected_terminal_output_${NAME}.txt 
+  TERMINAL_ERROR_CODE=$?
+  if ! test ${TERMINAL_ERROR_CODE} -eq 0;
+  then
+    echo "Error! File generated different terminal output!"
+    exit 1
+  fi
+
+  cmp output.csv ${THIS_DIR}/expected_csv_output_${NAME}.csv
+  CSV_ERROR_CODE=$?
+  if ! test ${CSV_ERROR_CODE} -eq 0;
+  then
+    echo "Error! File generated different values for output.csv"
+    exit 2
+  fi
+  # Reset back to original directory
   cd ${THIS_DIR} 
 done
 echo "All examples successfully executed!"
