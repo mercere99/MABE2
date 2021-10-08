3d67fd0157f75b40c6ae021bcf810b6cac7a3aea
Charles Ofria <charles.ofria@gmail.com>
2021-10-08 14:33:21 -0400

    Removed vectors of entires from scope; now all in map.

--- source/config/ConfigEntry_Scope.hpp
+++ source/config/ConfigEntry_Scope.hpp
@@ -27,9 +27,8 @@ namespace mabe {
   class ConfigEntry_Scope : public ConfigEntry {
   protected:
     using entry_ptr_t = emp::Ptr<ConfigEntry>;
-    emp::vector< entry_ptr_t > entry_list;            ///< Entries in order.
-    emp::vector< entry_ptr_t > builtin_list;          ///< Built-in entries; not in config.
-    emp::map< std::string, entry_ptr_t > entry_map;   ///< Entries with easy lookup.
+    using const_entry_ptr_t = emp::Ptr<const ConfigEntry>;
+    emp::map< std::string, entry_ptr_t > symbol_table;   ///< Map of names to entries.
 
     ///< If this scope represents a structure, identify the type (otherwise type is "")
     const std::string type;
@@ -37,48 +36,35 @@ namespace mabe {
     template <typename T, typename... ARGS>
     T & Add(const std::string & name, ARGS &&... args) {
       auto new_ptr = emp::NewPtr<T>(name, std::forward<ARGS>(args)...);
-      entry_list.push_back(new_ptr);
-      emp_assert(!emp::Has(entry_map, name), "Do not redeclare functions or variables!",
+      emp_assert(!emp::Has(symbol_table, name), "Do not redeclare functions or variables!",
                  name);
-      entry_map[name] = new_ptr;
+      symbol_table[name] = new_ptr;
       return *new_ptr;
     }
 
     template <typename T, typename... ARGS>
     T & AddBuiltin(const std::string & name, ARGS &&... args) {
-      auto new_ptr = emp::NewPtr<T>(name, std::forward<ARGS>(args)...);
-      builtin_list.push_back(new_ptr);
-      emp_assert(!emp::Has(entry_map, name), "Do not redeclare built-in functions or variables!",
-                 name);
-      entry_map[name] = new_ptr;
-      return *new_ptr;
+      T & result = Add<T, ARGS...>(name, std::forward<ARGS>(args)...);
+      result.SetBuiltin();
+      return result;
     }
+
   public:
     ConfigEntry_Scope(const std::string & _name,
                 const std::string & _desc,
                 emp::Ptr<ConfigEntry_Scope> _scope,
                 const std::string & _type="")
       : ConfigEntry(_name, _desc, _scope), type(_type) { }
+
     ConfigEntry_Scope(const ConfigEntry_Scope & in) : ConfigEntry(in) {
       // Copy all defined variables/scopes/functions
-      for (const auto & x : in.entry_list) {
-        auto new_ptr = x->Clone();
-        entry_list.push_back(new_ptr);
-        entry_map[x->GetName()] = new_ptr;
-      }
-      // Copy all built-in variables/scopes/functions
-      for (const auto & x : in.builtin_list) {
-        auto new_ptr = x->Clone();
-        builtin_list.push_back(new_ptr);
-        entry_map[x->GetName()] = new_ptr;
-      }
+      for (auto [name, ptr] : symbol_table) { symbol_table[name] = ptr->Clone(); }
     }
     ConfigEntry_Scope(ConfigEntry_Scope &&) = default;
 
     ~ConfigEntry_Scope() {
-      // Clear up all entries and built-ins.
-      for (auto & x : entry_list) { x.Delete(); }
-      for (auto & x : builtin_list) { x.Delete(); }
+      // Clear up the symbol table.
+      for (auto [name, ptr] : symbol_table) { ptr.Delete(); }
     }
 
     std::string GetTypename() const override { return type; }
@@ -90,49 +76,40 @@ namespace mabe {
     emp::Ptr<ConfigEntry_Scope> AsScopePtr() override { return this; }
 
     /// Get an entry out of this scope; 
-    entry_ptr_t GetEntry(std::string in_name) {
-      // Lookup this next entry is in the var list.
-      auto it = entry_map.find(in_name);
-
-      // If this name is unknown, fail!
-      if (it == entry_map.end()) return nullptr;
-
-      // Otherwise return the entry.
-      return it->second;
-    }
+    entry_ptr_t GetEntry(std::string name) { return emp::Find(symbol_table, name, nullptr); }
 
     /// Lookup a variable, scanning outer scopes if needed
-    entry_ptr_t LookupEntry(const std::string & in_name, bool scan_scopes=true) override {
+    entry_ptr_t LookupEntry(const std::string & name, bool scan_scopes=true) override {
       // See if this next entry is in the var list.
-      auto it = entry_map.find(in_name);
+      auto it = symbol_table.find(name);
 
       // If this name is unknown, check with the parent scope!
-      if (it == entry_map.end()) {
+      if (it == symbol_table.end()) {
         if (scope.IsNull() || !scan_scopes) return nullptr;  // No parent?  Just fail...
-        return scope->LookupEntry(in_name);
+        return scope->LookupEntry(name);
       }
 
       // Otherwise we found it!
       return it->second;
     }
 
-    /// Lookup a variable, scanning outer scopes if needed (in constant context!)
-    emp::Ptr<const ConfigEntry> LookupEntry(const std::string & in_name, bool scan_scopes=true) const override {
+    /// Lookup a variable, scanning outer scopes if needed (in const context!)
+    const_entry_ptr_t LookupEntry(const std::string & name, bool scan_scopes=true) const override {
       // See if this entry is in the var list.
-      auto it = entry_map.find(in_name);
+      auto it = symbol_table.find(name);
 
       // If this name is unknown, check with the parent scope!
-      if (it == entry_map.end()) {
+      if (it == symbol_table.end()) {
         if (scope.IsNull() || !scan_scopes) return nullptr;  // No parent?  Just fail...
-        return scope->LookupEntry(in_name);
+        return scope->LookupEntry(name);
       }
 
       // Otherwise we found it!
       return it->second;
     }
 
-    /// Link a variable to a configuration entry - it sets the new default and
-    /// automatically updates when configs are loaded.
+    /// Add a configuration entry that is linked to a variable - the incoming variable sets
+    /// the default and is automatically updated when configs are loaded.
     template <typename VAR_T>
     ConfigEntry_Linked<VAR_T> & LinkVar(const std::string & name,
                                         VAR_T & var,
@@ -142,8 +119,8 @@ namespace mabe {
       return Add<ConfigEntry_Linked<VAR_T>>(name, var, desc, this);
     }
 
-    /// Link a configuration entry to a pair of functions - it sets the new default and
-    /// automatically calls the set function when configs are loaded.
+    /// Add a configuration entry that interacts through a pair of functions - the functions
+    /// are automatically called any time the entry is accessed (get_fun) or changed (set_fun)
     template <typename VAR_T>
     ConfigEntry_LinkedFunctions<VAR_T> & LinkFuns(const std::string & name,
                                             std::function<VAR_T()> get_fun,
@@ -156,17 +133,17 @@ namespace mabe {
       return Add<ConfigEntry_LinkedFunctions<VAR_T>>(name, get_fun, set_fun, desc, this);
     }
 
-    /// Add a new variable of type String.
+    /// Add an internal variable of type String.
     ConfigEntry_StringVar & AddStringVar(const std::string & name, const std::string & desc) {
       return Add<ConfigEntry_StringVar>(name, "", desc, this);
     }
 
-    /// Add a new variable of type Value.
+    /// Add an internal variable of type Value.
     ConfigEntry_DoubleVar & AddValueVar(const std::string & name, const std::string & desc) {
       return Add<ConfigEntry_DoubleVar>(name, 0.0, desc, this);
     }
 
-    /// Add a new scope inside of this one.
+    /// Add an internal scope inside of this one.
     ConfigEntry_Scope & AddScope(const std::string & name, const std::string & desc, const std::string & type="") {
       return Add<ConfigEntry_Scope>(name, desc, this, type);
     }
@@ -192,8 +169,9 @@ namespace mabe {
                                       size_t comment_offset=32) const {
 
       // Loop through all of the entires in this scope and Write them.
-      for (auto x : entry_list) {
-        x->Write(os, prefix, comment_offset);
+      for (auto [name, ptr] : symbol_table) {
+        if (ptr->IsBuiltin()) continue; // Skip writing built-in entries.
+        ptr->Write(os, prefix, comment_offset);
       }
 
       return *this;
@@ -204,22 +182,24 @@ namespace mabe {
                               size_t comment_offset=32) const override
     {
       // If this is a built-in scope, don't print it.
-      if (IsBuiltIn()) return *this;
+      if (IsBuiltin()) return *this;
 
       // Declare this scope.
       std::string cur_line = prefix;
       if (IsLocal()) cur_line += emp::to_string(GetTypename(), " ");
       cur_line += name;
 
+      bool has_body = emp::AnyOf(symbol_table, [](entry_ptr_t ptr){ return !ptr->IsBuiltin(); }; );
+      
       // Only open this scope if there are contents.
-      cur_line += entry_list.size() ? " { " : ";";
+      cur_line += has_body ? " { " : ";";
       os << cur_line;
 
       // Indent the comment for the description (if there is one)
       WriteDesc(os, comment_offset, cur_line.size());
 
       // If we have internal entries, write them out.
-      if (entry_list.size()) {
+      if (has_body) {
         WriteContents(os, prefix+"  ", comment_offset);
         os << prefix << "}\n";      // Close the scope.
       }
