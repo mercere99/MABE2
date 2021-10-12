5107946cdd9fa612bdf60a339d924af24fcf88be
Charles Ofria <charles.ofria@gmail.com>
2021-10-12 14:54:05 -0400

    Built an easy-to-access base class for ConfigType.

---
+++ source/config/ConfigTypeBase.hpp
@@ -0,0 +1,52 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2021.
+ *
+ *  @file  ConfigTypeBase.hpp
+ *  @brief Base class for setting up custom types for use in scripting; usable throughout.
+ *  @note Status: ALPHA
+ */
+
+#ifndef MABE_CONFIG_TYPE_BASE_HPP
+#define MABE_CONFIG_TYPE_BASE_HPP
+
+#include "emp/base/assert.hpp"
+
+namespace mabe {
+
+  class ConfigEntry_Scope;
+  class ConfigTypeInfo;
+
+  enum class BaseType {
+    INVALID = 0, 
+    VOID,
+    VALUE,
+    STRING,
+    STRUCT
+  };
+
+  class ConfigTypeBase {
+  protected:
+    emp::Ptr<ConfigEntry_Scope> cur_scope;
+    emp::Ptr<ConfigTypeInfo> type_info_ptr;
+
+    // Some special, internal variables associated with each object.
+    bool _active=true;       ///< Should this object be used in the current run?
+    std::string _desc="";    ///< Special description for this object.
+
+  public:
+    virtual ~ConfigTypeBase() { }
+
+    virtual void SetupConfig() = 0;
+
+    ConfigEntry_Scope & GetScope() { emp_assert(!cur_scope.IsNull()); return *cur_scope; }
+    const ConfigEntry_Scope & GetScope() const { emp_assert(!cur_scope.IsNull()); return *cur_scope; }
+
+    ConfigTypeInfo & GetTypeInfo() { return *type_info_ptr; }
+    void SetTypeInfo( ConfigTypeInfo & _info ) { type_info_ptr = &_info; }
+  };
+
+}
+
+#endif
