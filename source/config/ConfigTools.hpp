bb73d07520938c5777036dd1ddfb167899a1907c
Charles Ofria <charles.ofria@gmail.com>
2021-10-24 23:16:31 -0400

    Cleaned up and finished implementing function wrapping to ConfigTools

--- source/config/ConfigTools.hpp
+++ source/config/ConfigTools.hpp
@@ -46,7 +46,7 @@ namespace ConfigTools {
     }
 
     // If a return value is a basic type, wrap it in a temporary entry
-    if constexpr (std::is_same<RETURN_T, std::string>() ||
+    else if constexpr (std::is_same<RETURN_T, std::string>() ||
                   std::is_arithmetic<RETURN_T>()) {
       return MakeTempEntry(return_value);
     }
@@ -60,16 +60,16 @@ namespace ConfigTools {
   }
 
 
-  template <typename FUN_T> struct WrapFunction_impl;
+  template <typename FUN_T, typename INDEX_Ts> struct WrapFunction_impl;
 
   // Specialization for functions with NO arguments
   template <typename RETURN_T>
-  struct WrapFunction_impl<RETURN_T()> {
+  struct WrapFunction_impl<RETURN_T(), emp::ValPack<>> {
 
     template <typename FUN_T>
-    static auto ConvertFun(FUN_T fun) {
-      return [fun=fun]([[maybe_unused]] const entry_vector_t & args) {
-        emp_assert(args.size() == 0, "Too many arguments (expected 0)", args.size());
+    static auto ConvertFun([[maybe_unused]] const std::string & name, FUN_T fun) {
+      return [name=name,fun=fun]([[maybe_unused]] const entry_vector_t & args) {
+        emp_assert(args.size() == 0, "Too many arguments (expected 0)", name, args.size());
         return ConvertReturn( fun() );
       };
     }
@@ -77,49 +77,25 @@ namespace ConfigTools {
   };
 
   // Specialization for functions with AT LEAST ONE argument.
-  template <typename RETURN_T, typename PARAM1_T, typename... PARAM_Ts>
-  struct WrapFunction_impl<RETURN_T(PARAM1_T, PARAM_Ts...)> {
+  template <typename RETURN_T, typename PARAM1_T, typename... PARAM_Ts, auto... INDEX_VALS>
+  struct WrapFunction_impl<RETURN_T(PARAM1_T, PARAM_Ts...), emp::ValPack<INDEX_VALS...>> {
     using this_fun_t = RETURN_T(PARAM1_T, PARAM_Ts...);
+    static_assert( sizeof...(PARAM_Ts) == sizeof...(INDEX_VALS),
+                   "Need one index for each parameter." );
 
     template <typename FUN_T>
-    static auto ConvertFun(FUN_T fun) {        
-      // If this function is already the correct type, just pass it along.
-      if constexpr (std::is_same<this_fun_t, target_t>()) { return fun; }
-
-      // Otherwise convert types as needed.
-      else {
-       return [fun=fun](const entry_vector_t & args) {
-          emp_assert(args.size() == 1+sizeof...(PARAM_Ts), "Wrong argument count!");
-          size_t i = 0;
-          return ConvertReturn( fun(args[i++]->As<PARAM1_T>(), args[i++]->As<PARAM_Ts>()...) );
-        };
-      }
-    }
-
-    template <typename FUN_T>
-    static auto ConvertMemberFun(const std::string & name, FUN_T fun) {  
-      constexpr size_t NUM_PARAMS = sizeof...(PARAM_Ts);
-
-      // If this function is already the correct type, just pass it along.
-      if constexpr (NUM_PARAMS == 1) {
-        using arg1_t = typename emp::FunInfo<FUN_T>::template arg_t<1>;
-        if (std::is_reference_v<PARAM1_T> &&
-            std::is_base_of_v<ConfigType, std::remove_reference_t<PARAM1_T>> &&
-            std::is_same_v<arg1_t, const entry_vector_t &>) {
-          return fun;
+    static auto ConvertFun(const std::string & name, FUN_T fun) {        
+      return [name=name,fun=fun](const entry_vector_t & args) {
+        // If this function already takes a const entry_vector_t & as its only parameter,
+        // just pass it along.
+        if constexpr (sizeof...(PARAM_Ts) == 0 &&
+                      std::is_same_v<PARAM1_T, const entry_vector_t &>) {
+          return ConvertReturn( fun(args) );
         }
-      }
 
-      // Otherwise convert types as needed.
-      else {
-       return [name=name,fun=fun](ConfigType & obj, const entry_vector_t & args) {
-          emp_assert(args.size() == sizeof...(PARAM_Ts), "Wrong argument count!",
-                     args.size(), sizeof...(PARAM_Ts));
-          emp::Ptr<ConfigType> obj_ptr(&obj);
-          auto typed_ptr = obj_ptr.DynamicCast<std::remove_reference_t<PARAM1_T>>();
-          emp_assert(typed_ptr, "Internal error: member function call on wrong object type!");
-
-          // Make sure we have the correct number of arguments.
+        // Otherwise make sure we have the correct arguments.
+        else {
+          constexpr size_t NUM_PARAMS = 1 + sizeof...(PARAM_Ts);
           if (args.size() != NUM_PARAMS) {
             std::cerr << "Error in call to function '" << name
               << "'; expected " << NUM_PARAMS
@@ -128,10 +104,62 @@ namespace ConfigTools {
           }
           //@CAO should collect file position information for the above errors.
 
-          size_t arg_id = 0;
-          return ConvertReturn( fun(*typed_ptr, args[arg_id++]->As<PARAM_Ts>()...) );
-        };
-      }
+          return ConvertReturn( fun(args[0]->As<PARAM1_T>(),
+                                args[INDEX_VALS]->template As<PARAM_Ts>()...) );
+        }
+      };      
+    }
+
+    template <typename FUN_T>
+    static auto ConvertMemberFun(const std::string & name, FUN_T fun) {  
+      using info_t = emp::FunInfo<FUN_T>;
+
+      static_assert(std::is_reference_v<PARAM1_T>,
+                    "First parameter for supplied member functions must be reference to object");
+      static_assert(std::is_base_of_v<ConfigType, std::remove_reference_t<PARAM1_T>>,
+                    "First parameter for supplied member functions must derived from ConfigType");
+      static_assert(info_t::num_args == sizeof...(PARAM_Ts) + 1,
+                    "PARAM_Ts must match the extra arguments in member function.");
+
+      return [name=name,fun=fun](ConfigType & obj, const entry_vector_t & args) {
+        // Make sure the correct object type is used for first argument.
+        emp::Ptr<ConfigType> obj_ptr(&obj);
+        auto typed_ptr = obj_ptr.DynamicCast<std::remove_reference_t<PARAM1_T>>();
+        emp_assert(typed_ptr, "Internal error: member function call on wrong object type!", name);
+
+        // If this member function takes no additional arguments just call it without any!
+        if constexpr (sizeof...(PARAM_Ts) == 0) {
+          if (args.size() != 0) {
+            std::cerr << "Error in call to function '" << name
+              << "'; expected ZERO arguments, but received " << args.size() << "."
+              << std::endl;
+          }
+          //@CAO should collect file position information for the above errors.
+
+          return ConvertReturn( fun(*typed_ptr) );
+        }
+
+        // If this function already takes a const entry_vector_t & as its only extra parameter,
+        // just pass it along.
+        else if constexpr (sizeof...(PARAM_Ts) == 1 &&
+                      std::is_same_v<typename info_t::template arg_t<1>, const entry_vector_t &>) {
+          return ConvertReturn( fun(*typed_ptr, args) );
+        }
+
+        // Otherwise make sure we have the correct arguments.
+        else {
+          constexpr size_t NUM_PARAMS = sizeof...(PARAM_Ts);
+          if (args.size() != NUM_PARAMS) {
+            std::cerr << "Error in call to function '" << name
+              << "'; expected " << NUM_PARAMS
+              << " arguments, but received " << args.size() << "."
+              << std::endl;
+          }
+          //@CAO should collect file position information for the above errors.
+
+          return ConvertReturn( fun(*typed_ptr, args[INDEX_VALS]->template As<PARAM_Ts>()...) );
+        }
+      };
     }
 
   };
@@ -139,8 +167,15 @@ namespace ConfigTools {
   // Wrap a provided function to make it take a vector of Ptr<ConfigEntry> and return a
   // single Ptr<ConfigEntry> representing the result.
   template <typename FUN_T>
-  static auto WrapFunction(FUN_T fun) {
-    return WrapFunction_impl<typename emp::FunInfo<FUN_T>::fun_t>::ConvertFun(fun);
+  static auto WrapFunction(const std::string & name, FUN_T fun) {
+    using info_t = emp::FunInfo<FUN_T>;
+    using fun_t = typename info_t::fun_t;
+    if constexpr (info_t::num_args == 0) {
+      return WrapFunction_impl<fun_t, emp::ValPack<>>::ConvertFun(name, fun);
+    } else {
+      using index_t = emp::ValPackCount<info_t::num_args-1>;
+      return WrapFunction_impl<fun_t, index_t>::ConvertFun(name, fun);
+    }
   }
 
   // Wrap a provided MEMBER function to make it take a reference to the object it is a member of
@@ -149,18 +184,19 @@ namespace ConfigTools {
   static auto WrapMemberFunction(emp::TypeID class_type, const std::string & name, FUN_T fun) {
     // Do some checks that will produce reasonable errors.
     using info_t = emp::FunInfo<FUN_T>;
+    using index_t = emp::ValPackCount<info_t::num_args-1>;
     static_assert(info_t::num_args >= 1, "Member function add must always begin with an object reference.");
-    using object_t = typename info_t::template arg_t<0>;
-    using base_object_t = typename std::remove_cv_t< std::remove_reference_t<object_t> >;
 
     // Is the first parameter the correct type?
+    using object_t = typename info_t::template arg_t<0>;
+    using base_object_t = typename std::remove_cv_t< std::remove_reference_t<object_t> >;
     static_assert(std::is_base_of<ConfigType, base_object_t>(),
                   "Member functions must take a reference to the associated ConfigType");
     emp_assert( class_type.IsType<base_object_t>(),
                 "First parameter must match class type of member function being created!",
                 emp::GetTypeID<object_t>(), class_type );
 
-    return WrapFunction_impl<typename emp::FunInfo<FUN_T>::fun_t>::ConvertMemberFun(name, fun);
+    return WrapFunction_impl<typename emp::FunInfo<FUN_T>::fun_t, index_t>::ConvertMemberFun(name, fun);
   }
 
 }
