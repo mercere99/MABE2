5ebd2ee21e3d173180747b170ec692ab99161bc0
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-11-01 13:52:13 -0400

    Add path following task + related config file

--- source/evaluate/games/EvalPathFollow.hpp
+++ source/evaluate/games/EvalPathFollow.hpp
@@ -20,12 +20,30 @@
 
 #include "../../core/MABE.hpp"
 #include "../../core/Module.hpp"
+#include "../../orgs/VirtualCPUOrg.hpp"
 
 #include "emp/io/File.hpp"
 #include "emp/datastructs/Matrix.hpp"
 #include "emp/math/MVector.hpp"
 
 namespace mabe {
+
+  struct PathFollowState{
+  public:
+    bool initialized = false;
+    size_t cur_map_idx;
+    emp::Matrix<bool> visited_tiles;
+    emp::MVector cur_pos;
+    double raw_score;
+    double normalized_score;
+    emp::MVector cur_dir;
+    uint32_t forward_val = 12;
+    uint32_t right_val = 14005;
+    uint32_t left_val = 199;
+    uint32_t empty_val = 27272;
+
+    PathFollowState() : visited_tiles(0,0) { ; }
+  };
 
   class PathFollowEvaluator{
   protected:
@@ -42,6 +60,7 @@ namespace mabe {
     emp::vector<emp::Matrix<Tile>> tile_map_vec;
     emp::vector<emp::MVector> start_pos_vec;
     emp::vector<emp::MVector> start_dir_vec;
+    emp::vector<size_t> path_length_vec; 
     size_t max_steps = 100;
   public: 
     using path_follow_decision_func_t = std::function< size_t(size_t tile) >;
@@ -54,6 +73,7 @@ namespace mabe {
       emp::Matrix<Tile> tile_map(num_rows, num_cols);
       emp::MVector start_pos({-1,-1});
       emp::MVector start_dir({0,0});
+      size_t path_length = 0;
       for(size_t row_idx = 0; row_idx < num_rows; ++row_idx){
         std::string & cur_line = file[row_idx];
         for(size_t col_idx = 0; col_idx < cur_line.size(); ++col_idx){
@@ -61,19 +81,23 @@ namespace mabe {
           Tile cur_tile = Tile::OUT_OF_BOUNDS;
           switch(cur_line[col_idx]){
             case '.':
-              cur_tile = Tile::RIGHT;
+              cur_tile = Tile::EMPTY;
               break;
             case '+':
               cur_tile = Tile::FORWARD;
+              path_length++;
               break;
             case 'L':
               cur_tile = Tile::LEFT;
+              path_length++;
               break;
             case 'R':
               cur_tile = Tile::RIGHT;
+              path_length++;
               break;
             case 'X':
               cur_tile = Tile::FINISH;
+              path_length++;
               break;
             case '^':
               cur_tile = Tile::START;
@@ -116,7 +140,9 @@ namespace mabe {
       start_pos_vec.push_back(start_pos);
       emp_assert(start_dir[0] != 0 || start_dir[1] != 0, "Map must contain START tile!"); 
       start_dir_vec.push_back(start_dir);
-      std::cout << "Map is " << num_rows << "x" << num_cols << "!" << std::endl;
+      path_length_vec.push_back(path_length);
+      std::cout << "Map is " << num_rows << "x" << num_cols << ", with " << 
+          path_length << " tiles!" << std::endl;
     }
 
     void LoadAllMaps(const std::string& map_filenames_str){
@@ -125,6 +151,90 @@ namespace mabe {
       for(auto filename : map_filename_vec){
         LoadMap(filename);
       }
+    }
+    
+    void InitializeState(PathFollowState& state){
+      state.initialized = true;
+      state.cur_map_idx = 0;
+      state.visited_tiles.ExpandTo(
+          tile_map_vec[state.cur_map_idx].num_rows(), 
+          tile_map_vec[state.cur_map_idx].num_cols(), false);
+      state.cur_pos = start_pos_vec[state.cur_map_idx];
+      state.cur_dir = start_dir_vec[state.cur_map_idx];
+      state.raw_score = 0;
+      state.normalized_score = 0;
+    }
+
+    double GetCurrentPosScore(PathFollowState& state){
+      // If we're off the path, decrement score
+      if(tile_map_vec[state.cur_map_idx].Get(state.cur_pos[1], state.cur_pos[0]) == Tile::EMPTY)
+        return -1;
+      // On a new tile of the path, add score
+      if(!state.visited_tiles.Get(state.cur_pos[1], state.cur_pos[0])){
+        state.visited_tiles.Set(state.cur_pos[1], state.cur_pos[0], true);
+        return 1;
+      }
+      return 0; // Otherwise we've seen this tile of the path before, do nothing
+    }
+
+    double EvalMove(PathFollowState& state, double scale_factor = 1){
+      if(!state.initialized) InitializeState(state);
+      emp::MVector new_pos = state.cur_pos + (state.cur_dir * scale_factor);
+      // Bounds check
+      if(   new_pos[0] < 0 || new_pos[0] >= tile_map_vec[state.cur_map_idx].num_rows() ||
+            new_pos[1] < 0 || new_pos[1] >= tile_map_vec[state.cur_map_idx].num_cols()){
+        new_pos = state.cur_pos;
+      }
+      double score = 0;
+      if(state.cur_pos != new_pos){
+        score = GetCurrentPosScore(state);
+      }
+      state.cur_pos = new_pos;
+      state.raw_score += score;
+      if(state.raw_score > 0)
+        state.normalized_score = state.raw_score / path_length_vec[state.cur_map_idx];
+      else state.normalized_score = 0;
+      //std::cout << state.cur_pos << " -> " << new_pos << "; " << 
+      //    state.raw_score << " => "<< state.normalized_score << std::endl;
+      return state.normalized_score;
+    }
+    
+    void RotateRight(PathFollowState& state){
+      if(!state.initialized) InitializeState(state);
+        double tmp_val = state.cur_dir[0];
+        state.cur_dir[0] = -1 * state.cur_dir[1];
+        state.cur_dir[1] = tmp_val;
+    }
+
+    void RotateLeft(PathFollowState& state){
+      if(!state.initialized) InitializeState(state);
+        double tmp_val = state.cur_dir[0];
+        state.cur_dir[0] = state.cur_dir[1];
+        state.cur_dir[1] = -1 * tmp_val;
+    }
+
+    uint32_t Sense(PathFollowState& state){
+      switch(tile_map_vec[state.cur_map_idx].Get(state.cur_pos[1], state.cur_pos[0])){
+        case Tile::EMPTY:
+          return state.empty_val;
+          break;
+        case Tile::LEFT:
+          return state.left_val;
+          break;
+        case Tile::RIGHT:
+          return state.right_val;
+          break;
+        case Tile::FORWARD:
+          return state.forward_val;
+          break;
+        case Tile::START:
+          return state.forward_val;
+          break;
+       default:
+          return 0;
+          break;
+      }
+      return 0;
     }
 
     double EvalTrial(path_follow_decision_func_t& decision_func, size_t map_idx, 
@@ -191,19 +301,24 @@ namespace mabe {
       } 
       return score; 
     }
-    
   };
+
 
   class EvalPathFollow : public Module {
 
   private:
     Collection target_collect;                  ///< Which organisms should we evaluate?
 
-    std::string input_trait = "input";          ///< Name of trait to put input values.
-    std::string output_trait = "output";        ///< Name of trait to find output values.
     std::string score_trait = "score";         ///< Name of trait for organism performance
-    std::string map_filenames="";               ///< ;-separated list map filenames to load.
+    std::string state_trait ="state";          ///< Name of trait that stores the path follow state
+    std::string map_filenames="";              ///< ;-separated list map filenames to load.
     PathFollowEvaluator evaluator;
+    size_t pop_id = 0;
+    std::function<void(VirtualCPUOrg&, const VirtualCPUOrg::inst_t&)> func_move;
+    std::function<void(VirtualCPUOrg&, const VirtualCPUOrg::inst_t&)> func_rotate_right;
+    std::function<void(VirtualCPUOrg&, const VirtualCPUOrg::inst_t&)> func_rotate_left;
+    std::function<void(VirtualCPUOrg&, const VirtualCPUOrg::inst_t&)> func_sense;
+    std::function<void(VirtualCPUOrg&, const VirtualCPUOrg::inst_t&)> func_move_back;
 
   public:
     EvalPathFollow(mabe::MABE & control,
@@ -218,59 +333,70 @@ namespace mabe {
 
     void SetupConfig() override {
       LinkCollection(target_collect, "target", "Which population(s) should we evaluate?");
-      LinkVar(input_trait, "input_trait", "Into which trait should input values be placed?");
-      LinkVar(output_trait, "output_trait", "Out of which trait should output values be read?");
       LinkVar(score_trait, "score_trait", "Which trait stores path following performance?");
+      LinkVar(state_trait, "state_trait", "Which trait stores organisms' path follow state?");
       LinkVar(map_filenames, "map_filenames", 
           "List of map files to load, separated by semicolons(;)");
     }
 
-    void SetupModule() override {
-      AddOwnedTrait<emp::vector<double>>(input_trait, "Input values (current board state)", emp::vector<double>({0.0}));
-      AddRequiredTrait<emp::vector<double>>(output_trait); // Output values (move to make)
-      AddOwnedTrait<double>(score_trait, "Path following score", 0.0);
-      evaluator.LoadAllMaps(map_filenames);
-    }
-
-
-    PathFollowEvaluator::path_follow_decision_func_t ToOrgFunc(mabe::Organism & org) {
-      return [this, &org](size_t cur_tile) {
-        // Setup the hardware with proper inputs.
-        org.GetTrait<emp::vector<double>>(input_trait) = {cur_tile};
-
-        // Run the code.
-        org.GenerateOutput();
-
-        emp::vector<double> results = org.GetTrait<emp::vector<double>>(output_trait);
-
-        // Determine the chosen move.
-        size_t best_move = 0;
-        const size_t move_cap = std::min<size_t>(results.size(), 4);
-        for (size_t i = 1; i < move_cap; i++) {
-          if (results[best_move] < results[i]) { best_move = i; }
-        }
-        return best_move;
-      };
-    }
-
-    void OnUpdate(size_t ud) override {
-      control.Verbose("UD ", ud, ": Running EvalPathFollow::OnUpdate()");
-
-      emp_assert(control.GetNumPopulations() >= 1);
-      // Loop through the living organisms in the target collection to evaluate each.
-      mabe::Collection alive_collect( target_collect.GetAlive() );
-
-      control.Verbose(" - ", alive_collect.GetSize(), " organisms found.");
-
-      size_t org_id = 0;
-      for (Organism & org : alive_collect) {
-        double & score = org.GetTrait<double>(score_trait);
-        auto org_func = ToOrgFunc(org);
-        score = evaluator.EvalTrial(org_func, 0);
-        std::cout << org_id << " ";
-        org_id++;
+    void SetupInstructions(){
+      ActionMap& action_map = control.GetActionMap(pop_id);
+      // Move
+      {
+        func_move = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& /*inst*/){
+          double score = evaluator.EvalMove(hw.GetTrait<PathFollowState>(state_trait));
+          hw.SetTrait<double>(score_trait, score);
+        };
+        Action& action = action_map.AddFunc<void, VirtualCPUOrg&, const VirtualCPUOrg::inst_t&>(
+            "sg-move", func_move);
+        action.data.AddVar<int>("inst_id", 27);
       }
-      std::cout << std::endl;
+      // Move backward
+      {
+        func_move_back = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& /*inst*/){
+          double score = evaluator.EvalMove(hw.GetTrait<PathFollowState>(state_trait), -1);
+          hw.SetTrait<double>(score_trait, score);
+        };
+        Action& action = action_map.AddFunc<void, VirtualCPUOrg&, const VirtualCPUOrg::inst_t&>(
+            "sg-move-back", func_move_back);
+        action.data.AddVar<int>("inst_id", 28);
+      }
+      // Rotate right 
+      {
+        func_rotate_right = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& /*inst*/){
+          evaluator.RotateRight(hw.GetTrait<PathFollowState>(state_trait));
+        };
+        Action& action = action_map.AddFunc<void, VirtualCPUOrg&, const VirtualCPUOrg::inst_t&>(
+            "sg-rotate-r", func_rotate_right);
+        action.data.AddVar<int>("inst_id", 29);
+      }
+      // Rotate left 
+      {
+        func_rotate_left = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& /*inst*/){
+          evaluator.RotateLeft(hw.GetTrait<PathFollowState>(state_trait));
+        };
+        Action& action = action_map.AddFunc<void, VirtualCPUOrg&, const VirtualCPUOrg::inst_t&>(
+            "sg-rotate-l", func_rotate_left);
+        action.data.AddVar<int>("inst_id", 30);
+      }
+      // Sense 
+      {
+        func_sense = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& inst){
+          uint32_t val = evaluator.Sense(hw.GetTrait<PathFollowState>(state_trait));
+          size_t reg_idx = inst.nop_vec.empty() ? 1 : inst.nop_vec[0];
+          hw.regs[reg_idx] = val;
+        };
+        Action& action = action_map.AddFunc<void, VirtualCPUOrg&, const VirtualCPUOrg::inst_t&>(
+            "sg-sense", func_sense);
+        action.data.AddVar<int>("inst_id", 31);
+      }
+    }
+
+    void SetupModule() override {
+      AddSharedTrait<double>(score_trait, "Path following score", 0.0);
+      AddOwnedTrait<PathFollowState>(state_trait, "Organism's path follow state", { }); 
+      evaluator.LoadAllMaps(map_filenames);
+      SetupInstructions();
     }
   };
 
