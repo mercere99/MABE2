677b12dab8a8cd9c3c12fc220ac18fec3dcd8994
Charles Ofria <charles.ofria@gmail.com>
2021-11-05 13:22:04 -0400

    Setup proper dependency on EmplodeType.

--- source/Emplode/Symbol_Object.hpp
+++ source/Emplode/Symbol_Object.hpp
@@ -13,11 +13,10 @@
 
 #include "emp/base/map.hpp"
 
+#include "EmplodeType.hpp"
 #include "Symbol_Scope.hpp"
 
 namespace emplode {
-
-  class EmplodeType;
 
   // Set of multiple config entries.
   class Symbol_Object : public Symbol_Scope {
@@ -34,9 +33,12 @@ namespace emplode {
                   bool _owned)
       : Symbol_Scope(_name, _desc, _scope), obj_ptr(_obj), obj_owned(_owned) { }
 
-    Symbol_Object(const Symbol_Object & in) : Symbol_Scope(in) {
+    Symbol_Object(const Symbol_Object & in) : Symbol_Scope(in) {       
       // Copy the internal object.
       // @CAO MUST DO THIS!!!!!!!!!!!!!!!!!!!!!
+
+      // Copy all defined variables/scopes/functions
+      for (auto [name, ptr] : symbol_table) { symbol_table[name] = ptr->Clone(); }
     }
     Symbol_Object(Symbol_Object && in)
       : Symbol_Scope(std::move(in)), obj_ptr(in.obj_ptr), obj_owned(in.obj_owned)
@@ -51,10 +53,18 @@ namespace emplode {
       if (obj_owned) obj_ptr.Delete();
     }
 
-    emp::Ptr<EmplodeType> GetObjectPtr()  override { return obj_ptr; }  
+    emp::Ptr<EmplodeType> GetObjectPtr() override { return obj_ptr; }  
     emp::Ptr<const EmplodeType> GetObjectPtr() const override { return obj_ptr; }  
 
     bool IsObject() const override { return true; }
+    emp::TypeID GetObjectType() {
+      if (obj_ptr.IsNull()) return emp::GetTypeID<void>();
+      return obj_ptr->GetTypeInfo().GetTypeID();
+    }
+
+    /// Set this symbol to be a correctly-typed scope pointer.
+    emp::Ptr<Symbol_Object> AsObjectPtr() override { return this; }
+    emp::Ptr<const Symbol_Object> AsObjectPtr() const override { return this; }
 
     /// Make a copy of this scope and all of the entries inside it.
     emp::Ptr<Symbol> Clone() const override { return emp::NewPtr<Symbol_Object>(*this); }
