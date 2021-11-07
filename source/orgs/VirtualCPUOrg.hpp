3289f0eb24f81ae554f32f028616a1fd022f09d1
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-11-07 17:05:59 -0500

    Add config option to load VCPU genome from file

--- source/orgs/VirtualCPUOrg.hpp
+++ source/orgs/VirtualCPUOrg.hpp
@@ -52,6 +52,7 @@ namespace mabe {
       std::string child_merit_name = "child_merit"; 
       double initial_merit = 0;
       bool verbose = false;
+      std::string initial_genome_filename = "ancestor.org";
 
       // Internal use
       emp::Binomial mut_dist;            ///< Distribution of number of mutations to occur.
@@ -93,9 +94,10 @@ namespace mabe {
       constexpr bool start_with_not = false;
       if (SharedData().init_random) Randomize(random);
       else{
-        std::cout << "Filling genome!" << std::endl;
-        for(size_t i = 0; i < 49; i++) PushInst("NopC");   
-        PushInst("Repro");
+        Load(SharedData().initial_genome_filename);
+        //std::cout << "Filling genome!" << std::endl;
+        //for(size_t i = 0; i < 49; i++) PushInst("NopC");   
+        //PushInst("Repro");
         //PushInst("HAlloc");  // 0  
         //PushInst("HSearch"); // 1
         //PushInst("NopC");    // 2
@@ -208,6 +210,8 @@ namespace mabe {
                       "Initial value for merit (task performance)");
       GetManager().LinkVar(SharedData().verbose, "verbose",
                       "If true, print execution info of organisms");
+      GetManager().LinkVar(SharedData().initial_genome_filename, "initial_genome_filename",
+                      "File that contains the gennome used to initialize organisms.");
     }
 
     /// Setup this organism type with the traits it need to track.
@@ -234,39 +238,6 @@ namespace mabe {
     }
 
     void SetupInstLib(){
-      /*
-      inst_lib_t& inst_lib = GetInstLib();
-      inst_lib.AddInst("Inc", inst_lib_t::Inst_Inc, 1, "Increment value in reg Arg1");
-      inst_lib.AddInst("Dec", inst_lib_t::Inst_Dec, 1, "Decrement value in reg Arg1");
-      inst_lib.AddInst("Not", inst_lib_t::Inst_Not, 1, "Logically toggle value in reg Arg1");
-      inst_lib.AddInst("SetReg", inst_lib_t::Inst_SetReg, 2, "Set reg Arg1 to numerical value Arg2");
-      inst_lib.AddInst("Add", inst_lib_t::Inst_Add, 3, "regs: Arg3 = Arg1 + Arg2");
-      inst_lib.AddInst("Sub", inst_lib_t::Inst_Sub, 3, "regs: Arg3 = Arg1 - Arg2");
-      inst_lib.AddInst("Mult", inst_lib_t::Inst_Mult, 3, "regs: Arg3 = Arg1 * Arg2");
-      inst_lib.AddInst("Div", inst_lib_t::Inst_Div, 3, "regs: Arg3 = Arg1 / Arg2");
-      inst_lib.AddInst("Mod", inst_lib_t::Inst_Mod, 3, "regs: Arg3 = Arg1 % Arg2");
-      inst_lib.AddInst("TestEqu", inst_lib_t::Inst_TestEqu, 3, "regs: Arg3 = (Arg1 == Arg2)");
-      inst_lib.AddInst("TestNEqu", inst_lib_t::Inst_TestNEqu, 3, "regs: Arg3 = (Arg1 != Arg2)");
-      inst_lib.AddInst("TestLess", inst_lib_t::Inst_TestLess, 3, "regs: Arg3 = (Arg1 < Arg2)");
-      inst_lib.AddInst("If", inst_lib_t::Inst_If, 2, "If reg Arg1 != 0, scope -> Arg2; else skip scope", emp::ScopeType::BASIC, 1);
-      inst_lib.AddInst("While", inst_lib_t::Inst_While, 2, "Until reg Arg1 != 0, repeat scope Arg2; else skip", emp::ScopeType::LOOP, 1);
-      inst_lib.AddInst("Countdown", inst_lib_t::Inst_Countdown, 2, "Countdown reg Arg1 to zero; scope to Arg2", emp::ScopeType::LOOP, 1);
-      inst_lib.AddInst("Break", inst_lib_t::Inst_Break, 1, "Break out of scope Arg1");
-      inst_lib.AddInst("Scope", inst_lib_t::Inst_Scope, 1, "Enter scope Arg1", emp::ScopeType::BASIC, 0);
-      inst_lib.AddInst("Define", inst_lib_t::Inst_Define, 2, "Build function Arg1 in scope Arg2", emp::ScopeType::FUNCTION, 1);
-      inst_lib.AddInst("Call", inst_lib_t::Inst_Call, 1, "Call previously defined function Arg1");
-      inst_lib.AddInst("Push", inst_lib_t::Inst_Push, 2, "Push reg Arg1 onto stack Arg2");
-      inst_lib.AddInst("Pop", inst_lib_t::Inst_Pop, 2, "Pop stack Arg1 into reg Arg2");
-      inst_lib.AddInst("Input", inst_lib_t::Inst_Input, 2, "Pull next value from input Arg1 into reg Arg2");
-      inst_lib.AddInst("Output", inst_lib_t::Inst_Output, 2, "Push reg Arg1 into output Arg2");
-      inst_lib.AddInst("CopyVal", inst_lib_t::Inst_CopyVal, 2, "Copy reg Arg1 into reg Arg2");
-      inst_lib.AddInst("ScopeReg", inst_lib_t::Inst_ScopeReg, 1, "Backup reg Arg1; restore at end of scope");
-
-      for (size_t i = 0; i < VirtualCPUOrg::CPU_SIZE; i++) {
-        GetInstLib().AddArg(emp::to_string((int)i), i);                   // Args can be called by value
-        GetInstLib().AddArg(emp::to_string("Reg", 'A'+(char)i), i);  // ...or as a register.
-      }
-      */
       LoadExternalInstructions();
     }
 
