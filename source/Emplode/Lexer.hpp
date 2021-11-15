917e16278a36e546b8a84f370bba58313e01fc8e
Charles Ofria <charles.ofria@gmail.com>
2021-11-15 10:24:05 -0500

    Setup Emplode lexer to accept strings with any types of quotes.

--- source/Emplode/Lexer.hpp
+++ source/Emplode/Lexer.hpp
@@ -20,7 +20,6 @@ namespace emplode {
     int token_identifier = -1;        ///< Token id for identifiers
     int token_number = -1;            ///< Token id for literal numbers
     int token_string = -1;            ///< Token id for literal strings
-    int token_char = -1;              ///< Token id for literal characters
     int token_dots = -1;              ///< Token id for a series of dots (...)
     int token_symbol = -1;            ///< Token id for other symbols
 
@@ -34,8 +33,7 @@ namespace emplode {
       // Meaningful tokens have next priority.
       token_identifier = AddToken("Identifier", "[a-zA-Z_][a-zA-Z0-9_]*");
       token_number = AddToken("Literal Number", "[0-9]+(\\.[0-9]+)?");
-      token_string = AddToken("Literal String", "\\\"([^\"\\\\]|\\\\.)*\\\"");
-      token_char = AddToken("Literal Character", "'([^'\n\\\\]|\\\\.)+'");
+      token_string = AddToken("Literal String", "(\\\"([^\"\\\\]|\\\\.)*\\\")|('([^'\\\\]|\\\\.)*')|(`([^`\\\\]|\\\\.)*`)");
       token_dots = AddToken("Dots", "\".\"+");
 
       /// Symbol tokens should have least priority.  They include any solitary character not listed
@@ -46,7 +44,6 @@ namespace emplode {
     bool IsID(const emp::Token token) const noexcept { return token.token_id == token_identifier; }
     bool IsNumber(const emp::Token token) const noexcept { return token.token_id == token_number; }
     bool IsString(const emp::Token token) const noexcept { return token.token_id == token_string; }
-    bool IsChar(const emp::Token token) const noexcept { return token.token_id == token_char; }
     bool IsDots(const emp::Token token) const noexcept { return token.token_id == token_dots; }
     bool IsSymbol(const emp::Token token) const noexcept { return token.token_id == token_symbol; }
   };
