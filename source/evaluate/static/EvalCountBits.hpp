794bc86f7ca96a534a9afb7f061adf871b35efd0
Charles Ofria <charles.ofria@gmail.com>
2021-11-16 17:01:26 -0500

    Updated EvalCountBits to new module setup.

--- source/evaluate/static/EvalCountBits.hpp
+++ source/evaluate/static/EvalCountBits.hpp
@@ -19,10 +19,8 @@ namespace mabe {
 
   class EvalCountBits : public Module {
   private:
-    Collection target_collect;
-
     std::string bits_trait;
-    std::string fitness_trait;
+    std::string score_trait;
     bool count_type;   // =0 for counts zeros, or =1 for count ones.
 
   public:
@@ -30,58 +28,64 @@ namespace mabe {
                   const std::string & name="EvalCountBits",
                   const std::string & desc="Evaluate bitstrings by counting ones (or zeros).",
                   const std::string & _btrait="bits",
-                  const std::string & _ftrait="fitness",
+                  const std::string & _ftrait="score",
                   bool _ctype=1)
       : Module(control, name, desc)
-      , target_collect(control.GetPopulation(0))
       , bits_trait(_btrait)
-      , fitness_trait(_ftrait)
+      , score_trait(_ftrait)
       , count_type(_ctype)
     {
       SetEvaluateMod(true);
     }
     ~EvalCountBits() { }
 
+    // Setup member functions associated with this class.
+    static void InitType(emplode::TypeInfo & info) {
+      info.AddMemberFunction("EVAL",
+                             [](EvalCountBits & mod, Collection list) { return mod.Evaluate(list); },
+                             "Count the ones in all orgs in an OrgList.");
+    }
+
     void SetupConfig() override {
-      LinkCollection(target_collect, "target", "Which population(s) should we evaluate?");
       LinkVar(bits_trait, "bits_trait", "Which trait stores the bit sequence to evaluate?");
-      LinkVar(fitness_trait, "fitness_trait", "Which trait should we store NK fitness in?");
+      LinkVar(score_trait, "score_trait", "Which trait should we store NK score in?");
       LinkVar(count_type, "count_type", "Which type of bit should we count? (0 or 1)");
     }
 
     void SetupModule() override {
       AddRequiredTrait<emp::BitVector>(bits_trait);
-      AddOwnedTrait<double>(fitness_trait, "All-ones fitness value", 0.0);
+      AddOwnedTrait<double>(score_trait, "All-ones score value", 0.0);
     }
 
-    void OnUpdate(size_t /* update */) override {
+    double Evaluate(Collection orgs) {
       emp_assert(control.GetNumPopulations() >= 1);
 
       // Loop through the population and evaluate each organism.
-      double max_fitness = 0.0;
+      double max_score = 0.0;
       emp::Ptr<Organism> max_org = nullptr;
-      mabe::Collection alive_collect( target_collect.GetAlive() );
+      mabe::Collection alive_collect( orgs.GetAlive() );
       for (Organism & org : alive_collect) {        
         // Make sure this organism has its bit sequence ready for us to access.
         org.GenerateOutput();
 
         // Count the number of ones in the bit sequence.
         const emp::BitVector & bits = org.GetTrait<emp::BitVector>(bits_trait);
-        double fitness = (double) bits.CountOnes();
+        double score = (double) bits.CountOnes();
 
         // If we were supposed to count zeros, subtract ones count from total number of bits.
-        if (count_type == 0) fitness = bits.size() - fitness;
+        if (count_type == 0) score = bits.size() - score;
 
-        // Store the count on the organism in the fitness trait.
-        org.SetTrait<double>(fitness_trait, fitness);
+        // Store the count on the organism in the score trait.
+        org.SetTrait<double>(score_trait, score);
 
-        if (fitness > max_fitness || !max_org) {
-          max_fitness = fitness;
+        if (score > max_score || !max_org) {
+          max_score = score;
           max_org = &org;
         }
       }
 
-      std::cout << "Max " << fitness_trait << " = " << max_fitness << std::endl;
+      std::cout << "Max " << score_trait << " = " << max_score << std::endl;
+      return max_score;
     }
   };
 
