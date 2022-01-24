a62bb119ef2408d20f169ecdf558bda662558c4b
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-01-24 13:48:24 -0500

    Add placeholder tests for Emplode

---
+++ tests/unit/Emplode/DataFile.cpp
@@ -0,0 +1,17 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2019-2021.
+ *
+ *  @file  DataFile.cpp
+ *  @brief TODO. Currently this is a placeholder so codecov will see the untested source code
+ */
+
+// CATCH
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// MABE
+#include "Emplode/DataFile.hpp"
+
+
+TEST_CASE("DataFile_Placeholder", "[Emplode]"){ ; }
