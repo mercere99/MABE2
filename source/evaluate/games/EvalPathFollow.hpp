1a1b313f074f47c6f35424bef183773357958e69
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-02-15 11:43:59 -0500

    Switch EvalPathFollow to use StateGrid + style

--- source/evaluate/games/EvalPathFollow.hpp
+++ source/evaluate/games/EvalPathFollow.hpp
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2021.
+ *  @date 2021-2022.
  *
  *  @file  EvalPathFollow.hpp
  *  @brief MABE Evaluation module that places organisms on the start of a path, and rewards 
@@ -13,6 +13,7 @@
  *    - Error checking
  *      - When loading map, it must have start and finish
  *      - When evaluating path, do some bounds checking on map_idx
+ *    - Docstrings
  */
 
 #ifndef MABE_EVAL_PATH_FOLLOW_HPP
@@ -21,29 +22,27 @@
 #include "../../core/MABE.hpp"
 #include "../../core/Module.hpp"
 #include "../../orgs/VirtualCPUOrg.hpp"
+#include "../../tools/StateGrid.hpp"
 
 #include "emp/io/File.hpp"
-//#include "emp/datastructs/Matrix.hpp"
 #include "emp/geometry/Point2D.hpp"
+#include "emp/bits/BitVector.hpp"
 
 namespace mabe {
-
-  template<typename T> 
-  struct Matrix : public emp::vector<emp::vector<T>>{ ; };
 
   struct PathFollowState{
   public:
     bool initialized = false;
     size_t cur_map_idx;
-    Matrix<bool> visited_tiles;
+    emp::BitVector visited_tiles; 
     emp::Point2D<int> cur_pos;
-    double raw_score;
+    double raw_score; // TODO: Be careful about mixing initialization in place vs constructor
     double normalized_score;
     emp::Point2D<int> cur_dir;
-    uint32_t forward_cue = 1;
-    uint32_t right_cue = 2;
+    uint32_t empty_cue = 1;
+    uint32_t forward_cue = 2;
     uint32_t left_cue = 3;
-    uint32_t empty_cue = 4;
+    uint32_t right_cue = 4; 
 
     PathFollowState() : cur_pos(-1,-1) { ; }
   };
@@ -54,98 +53,92 @@ namespace mabe {
       FORWARD,
       LEFT,
       RIGHT,
-      START,
+      START_UP,
+      START_DOWN,
+      START_LEFT,
+      START_RIGHT,
       FINISH,
       OUT_OF_BOUNDS
     };
 
-    PathFollowEvaluator() : rand_ptr(nullptr) { ; }
     
     size_t num_maps = 0;
-    emp::vector<Matrix<Tile>> tile_map_vec;
+    emp::vector<emp::StateGrid> tile_map_vec; // For cleanliness, make a TileMap class and  
+                                                // collapse these four vectors 
     emp::vector<emp::Point2D<int>> start_pos_vec;
     emp::vector<emp::Point2D<int>> start_dir_vec;
     emp::vector<size_t> path_length_vec; 
     size_t max_steps = 100;
-    emp::Ptr<emp::Random> rand_ptr = nullptr; 
+    emp::Ptr<emp::Random> rand_ptr;
     bool randomize_cues = true;
-    void LoadMap(const std::string& filename){
-      std::cout << "Loading map: " << filename << std::endl;
-      emp::File file(filename);
-      emp_assert(file.GetNumLines() > 0, "Loading empty/invalid file!");
-      file.RemoveWhitespace();
-      size_t num_rows = file.GetNumLines();
-      size_t num_cols = file[0].size();
-      Matrix<Tile> tile_map;
-      tile_map.resize(num_rows);
-      for(size_t row_idx = 0; row_idx < num_rows; ++row_idx){
-        tile_map[row_idx].resize(num_cols);
-      }
+    
+    PathFollowEvaluator() : rand_ptr(nullptr) { ; } 
+
+    template <typename... Ts>
+    void LoadMap(Ts &&... args){
+      emp::StateGrid grid;
+      grid.AddState(Tile::EMPTY,       '.', 1.0, "empty");
+      grid.AddState(Tile::FORWARD,     '+', 1.0, "forward");
+      grid.AddState(Tile::LEFT,        'L', 1.0, "turn_left");
+      grid.AddState(Tile::RIGHT,       'R', 1.0, "turn_right");
+      grid.AddState(Tile::FINISH,      'X', 1.0, "finish");
+      grid.AddState(Tile::START_UP,    '^', 1.0, "start_up");
+      grid.AddState(Tile::START_DOWN,  'v', 1.0, "start_down");
+      grid.AddState(Tile::START_LEFT,  '<', 1.0, "start_left");
+      grid.AddState(Tile::START_RIGHT, '>', 1.0, "start_right");
+
+      grid.Load(std::forward<Ts>(args)...);
       emp::Point2D<int> start_pos({-1,-1});
       emp::Point2D<int> start_dir({0,0});
       size_t path_length = 0;
-      for(size_t row_idx = 0; row_idx < num_rows; ++row_idx){
-        std::string & cur_line = file[row_idx];
-        for(size_t col_idx = 0; col_idx < cur_line.size(); ++col_idx){
-          std::cout << cur_line[col_idx]; 
-          Tile cur_tile = Tile::OUT_OF_BOUNDS;
-          switch(cur_line[col_idx]){
-            case '.':
-              cur_tile = Tile::EMPTY;
+      for(size_t row_idx = 0; row_idx < grid.GetHeight(); ++row_idx){
+        for(size_t col_idx = 0; col_idx < grid.GetWidth(); ++col_idx){
+          switch(grid.GetState(col_idx, row_idx)){
+            case Tile::EMPTY:
               break;
-            case '+':
-              cur_tile = Tile::FORWARD;
+            case Tile::FORWARD:
               path_length++;
               break;
-            case 'L':
-              cur_tile = Tile::LEFT;
+            case Tile::LEFT:
               path_length++;
               break;
-            case 'R':
-              cur_tile = Tile::RIGHT;
+            case Tile::RIGHT:
               path_length++;
               break;
-            case 'X':
-              cur_tile = Tile::FINISH;
+            case Tile::FINISH:
               path_length++;
               break;
-            case '^':
-              cur_tile = Tile::START;
-              start_pos.Set(row_idx, col_idx);
-              start_dir.Set(-1, 0);
-              break;
-            case 'v':
-              cur_tile = Tile::START;
-              start_pos.Set(row_idx, col_idx);
-              start_dir.Set(1, 0);
-              break;
-            case '>':
-              cur_tile = Tile::START;
-              start_pos.Set(row_idx, col_idx);
-              start_dir.Set(0, 1);
-              break;
-            case '<':
-              cur_tile = Tile::START;
-              start_pos.Set(row_idx, col_idx);
+            case Tile::START_UP: 
+              start_pos.Set(col_idx, row_idx);
               start_dir.Set(0, -1);
               break;
-            default:
-              emp_error("Unexpected symbol in path following map: ", cur_line[col_idx], 
-                  " from file: ", filename);
+            case Tile::START_DOWN:
+              start_pos.Set(col_idx, row_idx);
+              start_dir.Set(0, 1);
+              break;
+            case Tile::START_LEFT:
+              start_pos.Set(col_idx, row_idx);
+              start_dir.Set(-1, 0);
+              break;
+            case Tile::START_RIGHT:
+              start_pos.Set(col_idx, row_idx);
+              start_dir.Set(1, 0);
+              break;
           }
-          tile_map[row_idx][col_idx] = cur_tile;
         }
-        std::cout << std::endl;
       }
-      tile_map_vec.push_back(tile_map);
-      emp_assert(start_pos.GetX() != -1 && start_pos.GetY() != -1, "Map must contain START tile!"); 
+      tile_map_vec.push_back(grid);
+      emp_assert(start_pos.GetX() != -1 && start_pos.GetY() != -1, 
+          "Map must contain START tile!"); 
       start_pos_vec.push_back(start_pos);
-      emp_assert(start_dir.GetX() != 0 || start_dir.GetY() != 0, "Map must contain START tile!"); 
+      emp_assert(start_dir.GetX() != 0 || start_dir.GetY() != 0, 
+          "Map must contain START tile!"); 
       start_dir_vec.push_back(start_dir);
       path_length_vec.push_back(path_length);
       num_maps++;
-      std::cout << "Map #" << (num_maps - 1) << " is " << num_rows << "x" << num_cols << ", with " 
-        << path_length << " tiles!" << std::endl;
+      std::cout << "Map #" << (num_maps - 1) << " is " 
+        << grid.GetWidth() << "x" << grid.GetHeight() << ", with " 
+        << path_length << " path tiles!" << std::endl;
     }
 
     void LoadAllMaps(const std::string& map_filenames_str){
@@ -160,12 +153,8 @@ namespace mabe {
       state.initialized = true;
       if(reset_map) state.cur_map_idx = 0;
       emp_assert(num_maps > state.cur_map_idx, "Cannot initialize state before loading the map!");
-      state.visited_tiles.resize(tile_map_vec[state.cur_map_idx].size()); 
-      for(size_t row_idx = 0; row_idx < tile_map_vec[state.cur_map_idx].size(); ++row_idx){
-          state.visited_tiles[row_idx].resize(
-              tile_map_vec[state.cur_map_idx][row_idx].size(),
-              false);
-      }
+      state.visited_tiles.Resize(tile_map_vec[state.cur_map_idx].GetSize()); 
+      state.visited_tiles.Clear();
       state.cur_pos = start_pos_vec[state.cur_map_idx];
       state.cur_dir = start_dir_vec[state.cur_map_idx];
       state.raw_score = 0;
@@ -187,28 +176,32 @@ namespace mabe {
       }
     }
 
-    double GetCurrentPosScore(PathFollowState& state){
+    double GetCurrentPosScore(PathFollowState& state){ // Mark both arg and function const
       // If we're off the path, decrement score
-      Tile tile = tile_map_vec[state.cur_map_idx][state.cur_pos.GetX()][state.cur_pos.GetY()];
-      if(tile == Tile::EMPTY) return -1;
-      else if(tile == Tile::START) return 0;
+      int tile_id = tile_map_vec[state.cur_map_idx].GetState(state.cur_pos.GetX(), state.cur_pos.GetY());
+      if(tile_id == Tile::EMPTY) return -1;
       // On a new tile of the path, add score (forward, left, right, finish)
-      else if(!state.visited_tiles[state.cur_pos.GetX()][state.cur_pos.GetY()]){
+      else if(!state.visited_tiles[
+          (state.cur_pos.GetY() * tile_map_vec[state.cur_map_idx].GetWidth()) + 
+           state.cur_pos.GetX()
+      ]){
         return 1;
       }
       return 0; // Otherwise we've seen this tile of the path before, do nothing
     }
 
     void MarkVisited(PathFollowState& state){
-        state.visited_tiles[state.cur_pos.GetX()][state.cur_pos.GetY()] =  true;
+      state.visited_tiles.Set(
+          (state.cur_pos.GetY() * tile_map_vec[state.cur_map_idx].GetWidth()) + state.cur_pos.GetX(), 
+          true);
     }
 
     double Move(PathFollowState& state, int scale_factor = 1){
       if(!state.initialized) InitializeState(state);
       emp::Point2D<int> new_pos = state.cur_pos + (state.cur_dir * scale_factor);
       // Bounds check
-      if( new_pos.GetX() < 0 || new_pos.GetX() >= tile_map_vec[state.cur_map_idx].size() ||
-          new_pos.GetY() < 0 || new_pos.GetY() >= tile_map_vec[state.cur_map_idx][0].size()){
+      if(new_pos.GetX() < 0 || new_pos.GetX() >= tile_map_vec[state.cur_map_idx].GetWidth() ||
+         new_pos.GetY() < 0 || new_pos.GetY() >= tile_map_vec[state.cur_map_idx].GetHeight()){
         new_pos = state.cur_pos;
       }
       if(state.cur_pos == new_pos) return state.normalized_score;
@@ -223,22 +216,16 @@ namespace mabe {
     void RotateRight(PathFollowState& state){
       if(!state.initialized) InitializeState(state);
         state.cur_dir = state.cur_dir.GetRot90();
-        //double tmp_val = state.cur_dir[0];
-        //state.cur_dir[0] = -1 * state.cur_dir[1];
-        //state.cur_dir[1] = tmp_val;
     }
 
     void RotateLeft(PathFollowState& state){
       if(!state.initialized) InitializeState(state);
         state.cur_dir = state.cur_dir.GetRot270();
-        //double tmp_val = state.cur_dir[0];
-        //state.cur_dir[0] = state.cur_dir[1];
-        //state.cur_dir[1] = -1 * tmp_val;
     }
 
-    uint32_t Sense(PathFollowState& state){
+    uint32_t Sense(PathFollowState& state){ // Could be const
       if(!state.initialized) InitializeState(state);
-      switch(tile_map_vec[state.cur_map_idx][state.cur_pos.GetX()][state.cur_pos.GetY()]){
+      switch(tile_map_vec[state.cur_map_idx].GetState(state.cur_pos.GetX(), state.cur_pos.GetY())){
         case Tile::EMPTY:
           return state.empty_cue;
           break;
@@ -251,8 +238,17 @@ namespace mabe {
         case Tile::FORWARD:
           return state.forward_cue;
           break;
-        case Tile::START:
+        case Tile::START_UP:
           return state.forward_cue;
+          break;
+        case Tile::START_DOWN:
+          return state.forward_cue;
+          break;
+        case Tile::START_LEFT:
+          return state.left_cue;
+          break;
+        case Tile::START_RIGHT:
+          return state.right_cue;
           break;
         case Tile::FINISH:
           return state.forward_cue;
@@ -310,6 +306,7 @@ namespace mabe {
 
     void SetupInstructions(){
       ActionMap& action_map = control.GetActionMap(pop_id);
+      // TODO: make std::functions auto and then forget about them
       { // Move
         func_move = [this](VirtualCPUOrg& hw, const VirtualCPUOrg::inst_t& /*inst*/){
           double score = evaluator.Move(hw.GetTrait<PathFollowState>(state_trait));
