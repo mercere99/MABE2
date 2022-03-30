764140aeb7ad99046ab83ce519a65b97c503a24e
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-03-30 17:11:15 -0400

    Update name of AnnotatePlacement for unit test

--- tests/unit/placement/AnnotatePlacement_Position.cpp
+++ tests/unit/placement/AnnotatePlacement_Position.cpp
@@ -11,7 +11,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 // MABE
-#include "placement/AnnotatePlacement.hpp"
+#include "placement/AnnotatePlacement_Position.hpp"
 
 
-TEST_CASE("AnnotatePlacement_Placeholder", "[core]"){ ; }
+TEST_CASE("AnnotatePlacement_Position_Placeholder", "[core]"){ ; }
