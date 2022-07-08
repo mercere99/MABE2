ffb930b8019856afcecab1e9b6f992212330cfc1
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-07-08 14:37:43 -0400

    Update regression scripts to handle complex paths

--- tests/regression/regenerate_expected_outputs.sh
+++ tests/regression/regenerate_expected_outputs.sh
@@ -1,26 +1,26 @@
 MABE_BUILD_DIR="../../build" # Relative to THIS_DIR below
 
 THIS_DIR=`pwd`
-MABE_BUILD_DIR=${THIS_DIR}/${MABE_BUILD_DIR}
-LOCAL_BUILD_DIR=${THIS_DIR}/build
-PROJECTS=`ls ${THIS_DIR}/projects`
+MABE_BUILD_DIR="${THIS_DIR}/${MABE_BUILD_DIR}"
+LOCAL_BUILD_DIR="${THIS_DIR}/build"
+PROJECTS=`ls "${THIS_DIR}/projects"`
 
 set -e # If any errors occur, propogate error and stop
 
 ### Compile
-mkdir -p ${LOCAL_BUILD_DIR}
-cd ${MABE_BUILD_DIR}
+mkdir -p "${LOCAL_BUILD_DIR}"
+cd "${MABE_BUILD_DIR}"
 ## Debug mode
 make clean
 make debug 
 # Move executable
-mv ${MABE_BUILD_DIR}/MABE ${LOCAL_BUILD_DIR}/MABE_debug
+mv "${MABE_BUILD_DIR}/MABE" "${LOCAL_BUILD_DIR}/MABE_debug"
 ## Release mode
 make clean
 make 
 # Move executable
-mv ${MABE_BUILD_DIR}/MABE ${LOCAL_BUILD_DIR}/MABE
-cd ${THIS_DIR}
+mv "${MABE_BUILD_DIR}/MABE" "${LOCAL_BUILD_DIR}/MABE"
+cd "${THIS_DIR}"
 
 for NAME in ${PROJECTS}
 do
@@ -30,14 +30,14 @@ do
   OUTPUT_DIR="${PROJ_DIR}/output"
   FILE_DIR="${PROJ_DIR}/needed_files"
   EXPECTED_DIR="${PROJ_DIR}/expected"
-  mkdir ${OUTPUT_DIR} -p
-  mkdir ${EXPECTED_DIR} -p
-  mkdir ${FILE_DIR} -p
-  cd ${EXPECTED_DIR}
+  mkdir "${OUTPUT_DIR}" -p
+  mkdir "${EXPECTED_DIR}" -p
+  mkdir "${FILE_DIR}" -p
+  cd "${EXPECTED_DIR}"
   # Copy over mabe executable and all needed files
-  cp ${LOCAL_BUILD_DIR}/MABE ./
-  cp ${LOCAL_BUILD_DIR}/MABE_debug ./
-  cp ${FILE_DIR}/* ./
+  cp "${LOCAL_BUILD_DIR}/MABE" ./
+  cp "${LOCAL_BUILD_DIR}/MABE_debug" ./
+  cp "${FILE_DIR}"/* ./
   # Run!
   env MABE_IS_REGEN=1 ./run_regression_test.sh
   TERMINAL_ERROR_CODE=$?
@@ -47,15 +47,15 @@ do
     exit 1
   fi
   # Remove non-output files
-  rm ${EXPECTED_DIR}/MABE
-  rm ${EXPECTED_DIR}/MABE_debug
-  NEEDED_FILES=`ls ${FILE_DIR}`
+  rm "${EXPECTED_DIR}/MABE"
+  rm "${EXPECTED_DIR}/MABE_debug"
+  NEEDED_FILES=`ls "${FILE_DIR}"`
   for needed_file in ${NEEDED_FILES}
   do
-    rm ${EXPECTED_DIR}/${needed_file}
+    rm "${EXPECTED_DIR}/${needed_file}"
   done
   # Return to starting directory
-  cd ${THIS_DIR} 
+  cd "${THIS_DIR}"
 done
 echo "All expected data regenerated!"
 echo "Latest commit at time of last regeneration:" > last_test_regen_info.txt
--- tests/regression/run_examples.sh
+++ tests/regression/run_examples.sh
@@ -1,13 +1,13 @@
 MABE_BUILD_DIR="../../build" # Relative to THIS_DIR below
 
 THIS_DIR=`pwd`
-MABE_BUILD_DIR=${THIS_DIR}/${MABE_BUILD_DIR}
-LOCAL_BUILD_DIR=${THIS_DIR}/build
-PROJECTS=`ls ${THIS_DIR}/projects`
+MABE_BUILD_DIR="${THIS_DIR}/${MABE_BUILD_DIR}"
+LOCAL_BUILD_DIR="${THIS_DIR}/build"
+PROJECTS=`ls "${THIS_DIR}/projects"`
 
 ### Compile
-mkdir -p ${LOCAL_BUILD_DIR}
-cd ${MABE_BUILD_DIR}
+mkdir -p "${LOCAL_BUILD_DIR}"
+cd "${MABE_BUILD_DIR}"
 ## Debug mode
 make clean
 make debug 
@@ -19,7 +19,7 @@ then
   exit 1
 fi
 # Move executable
-mv ${MABE_BUILD_DIR}/MABE ${LOCAL_BUILD_DIR}/MABE_debug
+mv "${MABE_BUILD_DIR}/MABE" "${LOCAL_BUILD_DIR}/MABE_debug"
 ## Release mode
 make clean
 make 
@@ -31,8 +31,8 @@ then
   exit 1
 fi
 # Move executable
-mv ${MABE_BUILD_DIR}/MABE ${LOCAL_BUILD_DIR}/MABE
-cd ${THIS_DIR}
+mv "${MABE_BUILD_DIR}/MABE" "${LOCAL_BUILD_DIR}/MABE"
+cd "${THIS_DIR}"
 
 for NAME in ${PROJECTS}
 do
@@ -42,12 +42,12 @@ do
   OUTPUT_DIR="${PROJ_DIR}/output"
   FILE_DIR="${PROJ_DIR}/needed_files"
   EXPECTED_DIR="${PROJ_DIR}/expected"
-  mkdir ${OUTPUT_DIR} -p
-  cd ${OUTPUT_DIR}
+  mkdir "${OUTPUT_DIR}" -p
+  cd "${OUTPUT_DIR}"
   # Copy over mabe executable and all needed files
-  cp ${LOCAL_BUILD_DIR}/MABE ./
-  cp ${LOCAL_BUILD_DIR}/MABE_debug ./
-  cp ${FILE_DIR}/* ./
+  cp "${LOCAL_BUILD_DIR}/MABE" ./
+  cp "${LOCAL_BUILD_DIR}/MABE_debug" ./
+  cp "${FILE_DIR}"/* ./
   # Run!
   env MABE_IS_REGEN=0 ./run_regression_test.sh
   TERMINAL_ERROR_CODE=$?
@@ -57,23 +57,23 @@ do
     exit 1
   fi
   # Remove non-output files
-  rm ${OUTPUT_DIR}/MABE
-  rm ${OUTPUT_DIR}/MABE_debug
-  NEEDED_FILES=`ls ${FILE_DIR}`
+  rm "${OUTPUT_DIR}/MABE"
+  rm "${OUTPUT_DIR}/MABE_debug"
+  NEEDED_FILES=`ls "${FILE_DIR}"`
   for needed_file in ${NEEDED_FILES}
   do
-    rm ${OUTPUT_DIR}/${needed_file}
+    rm "${OUTPUT_DIR}/${needed_file}"
   done
   # Check generated data against expected data
-  EXPECTED_FILES=`ls ${EXPECTED_DIR}`
+  EXPECTED_FILES=`ls "${EXPECTED_DIR}"`
   for expected_file in ${EXPECTED_FILES}
   do
-    if ! [ -e ${OUTPUT_DIR}/${expected_file} ]
+    if ! [ -e "${OUTPUT_DIR}/${expected_file}" ]
     then
       echo "Error! Expected file not found in output! (${expected_file})"
       exit 1
     fi
-    cmp ${expected_file}  ${EXPECTED_DIR}/${expected_file}
+    cmp ${expected_file}  "${EXPECTED_DIR}/${expected_file}"
     TERMINAL_ERROR_CODE=$?
     if ! test ${TERMINAL_ERROR_CODE} -eq 0;
     then
@@ -84,7 +84,7 @@ do
     fi
   done
   # Reset back to original directory
-  cd ${THIS_DIR} 
+  cd "${THIS_DIR}" 
 done
 echo "All examples successfully executed!"
 # Placeholder comment
