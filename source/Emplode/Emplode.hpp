309c9831b1a9256536c96ea169c4a5d12753270b
Charles Ofria <charles.ofria@gmail.com>
2022-07-08 11:05:58 -0400

    Setup Emplode to load from either a single statement or a vector of statements.

--- source/Emplode/Emplode.hpp
+++ source/Emplode/Emplode.hpp
@@ -253,7 +253,7 @@ namespace emplode {
     SymbolTable & GetSymbolTable() { return symbol_table; }
     const SymbolTable & GetSymbolTable() const { return symbol_table; }
 
-    // Load a single, specified configuration file.
+    /// Load a single, specified configuration file.
     void Load(const std::string & filename) {
       std::ifstream file(filename);           // Load the provided file.
       emp::TokenStream tokens = lexer.Tokenize(file, filename);          // Convert to more-usable tokens.
@@ -271,15 +271,16 @@ namespace emplode {
       cur_block->Process();
     }
 
-    // Sequentially load a series of configuration files.
+    /// Sequentially load a series of configuration files.
     void Load(const emp::vector<std::string> & filenames) {
       for ( const std::string & fn : filenames) Load(fn);
     }
 
-    // Load a single, specified configuration file.
-    // @param statements List is statements to be parsed.
-    // @param name Name of statement group (for error messages)
-    void LoadStatements(const emp::vector<std::string> & statements, const std::string & name) {
+    /// Load a single, specified configuration file.
+    /// @param statements Statement (std::string) or vector of statements to be parsed.
+    /// @param name Name of statement group (for error messages)
+    template <typename STATEMENT_T>
+    void LoadStatements(const STATEMENT_T & statements, const std::string & name) {
       emp::TokenStream tokens = lexer.Tokenize(statements, name);    // Convert to tokens.
       pos_t pos = tokens.begin();
 
