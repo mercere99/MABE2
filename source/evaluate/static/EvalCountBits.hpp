18de96cea6cafb0ea20c0ec9bf54960f57edf8f4
Charles Ofria <charles.ofria@gmail.com>
2022-07-26 12:33:29 -0400

    Shifted EvalCountBits module to use trait objects.

--- source/evaluate/static/EvalCountBits.hpp
+++ source/evaluate/static/EvalCountBits.hpp
@@ -19,21 +19,16 @@ namespace mabe {
 
   class EvalCountBits : public Module {
   private:
-    std::string bits_trait;
-    std::string score_trait;
+    RequiredTrait<emp::BitVector> bits_trait{"bits", "Bit-sequence to evaluate."};
+    OwnedTrait<double> score_trait{"score", "Count of the number of specified bits"};
+
     bool count_type;   // =0 for counts zeros, or =1 for count ones.
 
   public:
     EvalCountBits(mabe::MABE & control,
                   const std::string & name="EvalCountBits",
-                  const std::string & desc="Evaluate bitstrings by counting ones (or zeros).",
-                  const std::string & _btrait="bits",
-                  const std::string & _ftrait="score",
-                  bool _ctype=1)
+                  const std::string & desc="Evaluate bitstrings by counting ones (or zeros).")
       : Module(control, name, desc)
-      , bits_trait(_btrait)
-      , score_trait(_ftrait)
-      , count_type(_ctype)
     {
       SetEvaluateMod(true);
     }
@@ -47,14 +42,13 @@ namespace mabe {
     }
 
     void SetupConfig() override {
-      LinkVar(bits_trait, "bits_trait", "Which trait stores the bit sequence to evaluate?");
-      LinkVar(score_trait, "score_trait", "Which trait should we store NK score in?");
       LinkVar(count_type, "count_type", "Which type of bit should we count? (0 or 1)");
+      RegisterTrait(bits_trait);
+      RegisterTrait(score_trait);
     }
 
     void SetupModule() override {
-      AddRequiredTrait<emp::BitVector>(bits_trait);
-      AddOwnedTrait<double>(score_trait, "All-ones score value", 0.0);
+      // Nothing needed for now.
     }
 
     double Evaluate(Collection orgs) {
@@ -69,14 +63,14 @@ namespace mabe {
         org.GenerateOutput();
 
         // Count the number of ones in the bit sequence.
-        const emp::BitVector & bits = org.GetTrait<emp::BitVector>(bits_trait);
+        const emp::BitVector & bits = bits_trait.Get(org);
         double score = (double) bits.CountOnes();
 
         // If we were supposed to count zeros, subtract ones count from total number of bits.
         if (count_type == 0) score = bits.size() - score;
 
         // Store the count on the organism in the score trait.
-        org.SetTrait<double>(score_trait, score);
+        score_trait(org) = score;
 
         if (score > max_score || !max_org) {
           max_score = score;
@@ -84,7 +78,7 @@ namespace mabe {
         }
       }
 
-      std::cout << "Max " << score_trait << " = " << max_score << std::endl;
+      std::cout << "Max " << score_trait.GetName() << " = " << max_score << std::endl;
       return max_score;
     }
   };
