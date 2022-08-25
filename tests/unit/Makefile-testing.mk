09c906a29fbc2b97c29addf1ca1689229695b773
Charles Ofria <charles.ofria@gmail.com>
2022-08-25 16:00:05 -0400

    Added a core file for unit tests

---
+++ tests/unit/Makefile-testing.mk
@@ -0,0 +1,28 @@
+MABE_DIR = $(TESTING_DIR)/../..
+default: test
+
+include $(MABE_DIR)/Makefile.base
+
+FLAGS = $(FLAGS_DEBUG)
+
+######## TESTING
+test-prep:
+	mkdir -p temp
+
+test-%: %.cpp $(CATCH_DIR)/catch.hpp
+	$(CXX) $(FLAGS) $< -o $@.out
+	./$@.out
+
+test: test-prep $(addprefix test-, $(TEST_NAMES))
+	rm -rf temp
+
+######## CODE COVERAGE
+cov-%: %.cpp ${CATCH_DIR}/catch.hpp
+	$(CXX) $(FLAGS_COVERAGE) $< -o $@.out
+	./$@.out
+	llvm-profdata merge default.profraw -o default.profdata
+	llvm-cov show ./$@.out -instr-profile=default.profdata > coverage_$@.txt
+	python ${EMP_DIR}/third-party/force-cover/fix_coverage.py coverage_$@.txt
+
+coverage: test-prep $(addprefix cov-, $(TEST_NAMES))
+
