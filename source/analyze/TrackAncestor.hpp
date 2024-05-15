3895183c00bf94e39e0d6aa424e7d3a3dde02918
Charles Ofria <charles.ofria@gmail.com>
2024-05-15 11:17:38 -0400

    Added module to track clades from initial injection.

---
+++ source/analyze/TrackAncestor.hpp
@@ -0,0 +1,53 @@
+/**
+ *  @note This file is part of MABE, https://github.com/mercere99/MABE2
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2024
+ *
+ *  @file  TrackAncestor.hpp
+ *  @brief MABE module to track info about the ancestor of each organism.
+ */
+
+#ifndef MABE_TRACK_ANCESTOR_HPP
+#define MABE_TRACK_ANCESTOR_HPP
+
+#include "../core/MABE.hpp"
+#include "../core/Module.hpp"
+
+namespace mabe {
+
+  /// Track information about an organism's ancestor.
+  class TrackAncestor : public Module {
+  private:
+    OwnedTrait<double> inject_time{this, "inject_time", "Update this lineage was injected"};
+    OwnedTrait<size_t> clade_id{this, "clade_id", "Unique ID for the clade from this ancestor"};
+
+    size_t next_clade = 0; // What value should the next clade ID have?
+
+  public:
+    TrackAncestor(mabe::MABE & control,
+                const std::string & name="TrackAncestor",
+                const std::string & desc="Module to choose the top fitness organisms for replication.")
+      : Module(control, name, desc)
+    { SetAnalyzeMod(true); } 
+    ~TrackAncestor() { }
+
+    // Setup member functions associated with this class.
+    static void InitType(emplode::TypeInfo &) {
+    }
+
+    void SetupConfig() override {
+    }
+
+    void SetupModule() override {
+    }
+
+    void OnInjectReady(Organism & org, Population &) override {
+      inject_time(org) = static_cast<double>(control.GetUpdate());
+      clade_id(org) = next_clade++;
+    }
+  };
+
+  MABE_REGISTER_MODULE(TrackAncestor, "Track info about the original ancestor of each organism.");
+}
+
+#endif
