7b8d92e1d86bf3f94c50ee0aa54378c2a2088c36
Charles Ofria <charles.ofria@gmail.com>
2024-05-17 15:18:33 -0400

    Overhaul of EvalLexicase to allow a major trait and ability to ensure all regular traits can go first.

--- source/select/SelectLexicase.hpp
+++ source/select/SelectLexicase.hpp
@@ -1,7 +1,7 @@
 /**
  *  @note This file is part of MABE, https://github.com/mercere99/MABE2
  *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
- *  @date 2021-2022.
+ *  @date 2021-2024.
  *
  *  @file  SelectLexicase.hpp
  *  @brief MABE module to enable Lexicase selection
@@ -31,34 +31,37 @@ namespace mabe {
     double epsilon = 0.0;       ///< Range from max value to be preserved? (fraction of max)
     size_t sample_traits = 0;   ///< Number of test cases to use each generation (0=off)
 
+    std::string major_trait;    ///< Is there a trait we want to emphasize in importance?
+    size_t major_range=10;      ///< Major trait guaranteed to be in first X tests.
+
+    int require_first=0;        ///< Do we require each test to be picked first at least once?
+
     Collection Select(Population & select_pop, Population & birth_pop, size_t num_births) {
       if (num_births > 1 && select_pop.GetID() == birth_pop.GetID()) {
-        emp::notify::Error("SelectLexicase requires birth_pop and select_pop to be different if selecting multiple organisms.");
+        emp::notify::Error("SelectLexicase currently requires birth_pop and select_pop to be different if selecting multiple organisms.");
         return Collection();
+      }
+      if (select_pop.IsEmpty()) return Collection();  // No living orgs!!
+
+      const size_t live_id = select_pop.FindOccupiedPos();
+      const size_t num_traits = trait_set.CountValues(select_pop[live_id].GetDataMap());
+      emp::Random & random = control.GetRandom();
+      const size_t major_count = (major_trait.size()) ? 1 : 0;
+
+      // If we're not using all of the traits, determine which ones to select on.
+      emp::vector<size_t> traits_used;
+      if (sample_traits) {
+        emp::Choose(random, num_traits-major_count, sample_traits, traits_used);
       }
 
       // Build a trait vector to hold the scores for each organism.
       emp::vector< emp::vector<double> > trait_scores(select_pop.GetSize());
 
-      // Find a living organism to setup traits.
-      size_t live_id = 0;
-      while (select_pop.IsEmpty(live_id)) live_id++;
-      if (live_id == select_pop.size()) return Collection();  // No living orgs!!
-      size_t num_traits = trait_set.CountValues(select_pop[live_id].GetDataMap());
-
-      emp::Random & random = control.GetRandom();
-
-      // If we're not using all of the traits, determine which ones to select on.
-      emp::vector<size_t> traits_used;
-      if (sample_traits) emp::Choose(random, num_traits, sample_traits, traits_used);
-
       // Loop through each organism to collect its trait information.
       emp::vector<size_t> start_orgs;
-      for (size_t org_id = live_id; org_id < num_births; ++org_id) {
+      for (size_t org_id = live_id; org_id < select_pop.GetSize(); ++org_id) {
         if (select_pop.IsEmpty(org_id)) continue;  // Skip empty positions in the population.
-
-        // This cell is not empty so add it to the full set of organisms.
-        start_orgs.push_back(org_id);
+        start_orgs.push_back(org_id);  // Add cell ID to the set of starting organisms.
 
         // Collect all of the trait values for the current organism.
         // If we are using a subset of traits, take that into account.
@@ -70,7 +73,7 @@ namespace mabe {
           // @CAO: This should be a user error, not a program error:
           emp_assert(num_traits == trait_scores[org_id].size(),
                     org_id, num_traits, trait_scores[org_id].size(),
-                    "All organisms need to have the same number of traits!");
+                    "All organisms must have the same number of traits!");
         }
       }
 
@@ -80,13 +83,22 @@ namespace mabe {
 
       // Create the correct number of offspring.
       Collection placement_list;
+      emp::vector<size_t> traits_order;
       for (size_t birth_id = 0; birth_id < num_births; ++birth_id) {
-        cur_orgs = start_orgs;              // For each offspring, start with full population
-        emp::Shuffle(random, traits_used);  // Shuffle traits into a random order.
+        traits_order = traits_used;
+        emp::Shuffle(random, traits_order);  // Shuffle traits into a random order.
+        if (major_trait.size()) {            // Insert the major trait if we are using one.
+          size_t major_pos = random.GetUInt(major_range);
+          traits_order.insert(traits_order.begin()+major_pos, num_traits-1);
+        }
+        if (require_first && birth_id < num_traits) {  // Give each or a turn first, if needed.
+          traits_order.insert(traits_order.begin(), birth_id);
+        }
 
-        // Step through traits and filter based on each.
-        for (size_t trait_id : traits_used) {
-          // Find the minimum and maximum values of the current trait.
+        // Step through traits and filter based on each trait.
+        cur_orgs = start_orgs;                              // Start with full population
+        for (size_t i = 0; i < traits_order.size(); ++i) {
+          size_t trait_id = traits_order[i];
           double min_value = std::numeric_limits<double>::max();
           double max_value = std::numeric_limits<double>::lowest();
           for (size_t org_id : cur_orgs) {
@@ -115,7 +127,7 @@ namespace mabe {
 
         emp_assert(cur_orgs.size() > 0);
 
-        // If there's only one organism left, replicate it!
+        // If there's only one organism left, mark it for replication.
         if (cur_orgs.size() == 1) {
           placement_list += control.Replicate(select_pop.IteratorAt(cur_orgs[0]), birth_pop);
         }
@@ -155,6 +167,9 @@ namespace mabe {
       LinkVar(trait_inputs, "fitness_traits", "Which traits provide the fitness values to use?");
       LinkVar(epsilon, "epsilon", "Range from max value to be preserved? (fraction of max)");
       LinkVar(sample_traits, "sample_traits", "Number of test cases to use each generation (0=all)" );
+      LinkVar(major_trait, "major_trait", "Is there a particular trait we want to emphasize?");
+      LinkVar(major_range, "major_range", "Major trait guaranteed to be in first X tests");
+      LinkVar(require_first, "require_first", "Require each test to be first at least once? (0=off; 1=on)");
     }
 
     void SetupModule() override {
@@ -162,15 +177,21 @@ namespace mabe {
       if (epsilon <= 0.0) epsilon = 0.000000001; // One billionth.
 
       // All of the traits used are required to be generated by another module.
-      emp::vector<std::string> trait_names = emp::slice(trait_inputs);
+      emp::vector<std::string> trait_names = emp::slice(trait_inputs, ',');
       for (const std::string & name : trait_names) {
         AddRequiredTrait<double, emp::vector<double>>(name, TraitInfo::ANY_COUNT);
+      }
+
+      // Check for the the major trait, if we have one.
+      if (major_trait.size()) {
+        AddRequiredTrait<double>(major_trait, 1);
       }
     }
 
     void SetupDataMap(emp::DataMap & dmap) override {
       trait_set.SetLayout(dmap.GetLayout()); ///< Give this trait set a layout to optimize.
       trait_set.SetTraits(trait_inputs);     ///< Parse set of trait inputs passed in.
+      if (major_trait.size()) { trait_set.AddTrait(major_trait); } // Add major trait at end, if any
     }
 
 
