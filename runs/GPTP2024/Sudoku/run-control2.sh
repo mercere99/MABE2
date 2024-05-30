454d03b890b60810498b9d03234f718c6923d584
Charles Ofria <charles.ofria@gmail.com>
2024-05-30 09:18:45 -0400

    Setup shell scripts to be executable.

--- runs/GPTP2024/Sudoku/run-control2.sh
+++ runs/GPTP2024/Sudoku/run-control2.sh
@@ -1,3 +1,4 @@
+#!/bin/tcsh
 foreach i ( 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 )
   mkdir control-1$i
   cd control-1$i
--- runs/GPTP2024/Sudoku/run2.sh
+++ runs/GPTP2024/Sudoku/run2.sh
@@ -1,3 +1,4 @@
+#!/bin/tcsh
 foreach i ( 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 )
   mkdir age-1$i
   cd age-1$i
