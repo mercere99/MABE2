c212b417509893e114a533e570342173ec131507
Charles Ofria <charles.ofria@gmail.com>
2024-06-26 15:56:30 -0400

    Update Sudoku configs to ones used.

--- runs/GPTP2024/Sudoku/analyze.sh
+++ runs/GPTP2024/Sudoku/analyze.sh
@@ -3,5 +3,5 @@
 /Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 1 control-???/max_org.csv > control-summary.csv
 /Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 1 -a age-???/max_org.csv > age-full.csv
 /Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 1 -a control-???/max_org.csv > control-full.csv
-/Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 3 -a age-???/max_org.csv > age-births.csv
-/Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 3 -a control-???/max_org.csv > control-births.csv
+/Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 7 -a age-???/max_org.csv > age-births.csv
+/Users/charles/Dropbox/Development/Empirical/demos/utils/data/summarize-change 7 -a control-???/max_org.csv > control-births.csv
